b4f11eec74881758c8b1d632d67aba33501c0be7
NiNinanana <yeen00123@gmail.com>
2023-12-31 18:53:13 +0900

    feat: 명문교회 이미지 수용 가능하게 설정

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "www.myungmoon.or.kr" }],
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
