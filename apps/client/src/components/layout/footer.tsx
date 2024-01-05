3fcc64df4103feac786a6f67fc207ff2ac5381cc
wannabeing <wannabeing@naver.com>
2024-01-06 02:45:59 +0900

    Footer 배경색 변경

--- apps/client/src/components/layout/footer.tsx
+++ apps/client/src/components/layout/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-indigo-100 h-[250px] flex justify-center">
+    <footer className="bg-[#dfc7c7] h-[250px] flex justify-center">
       <div className="max-w-[1100px] w-full flex items-center h-full gap-10">
         <div className="relative w-[300px] h-[100px]">
           <Image src="https://www.myungmoon.or.kr/img/body/logo.png" fill={true} alt="myungmoon" />
