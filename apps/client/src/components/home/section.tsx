6358f0ab1b07b2c000b6ea669df09be9ae55eee0
wannabeing <wannabeing@naver.com>
2024-01-19 20:54:13 +0900

    chore: Section 컴포넌트 props명 간소화

--- apps/client/src/components/home/section.tsx
+++ apps/client/src/components/home/section.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from "react";
 
 interface ISectionProps {
   children: ReactNode;
-  propsClassName?: string;
+  className?: string;
 }
-export default function Section({ children, propsClassName }: ISectionProps) {
-  return <div className={`flex flex-col gap-10 xl:gap-20 ${propsClassName}`}>{children}</div>;
+export default function Section({ children, className }: ISectionProps) {
+  return <div className={`flex flex-col gap-10 xl:gap-20 ${className}`}>{children}</div>;
 }
