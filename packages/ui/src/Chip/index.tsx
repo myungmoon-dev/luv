ffb6f6b8eee47b7933a950fa3227a6a2f8ef1d42
wannabeing <wannabeing@naver.com>
2024-02-19 01:04:37 +0900

    chore: Chip 컴포넌트 수정

--- packages/ui/src/Chip/index.tsx
+++ packages/ui/src/Chip/index.tsx
@@ -1,43 +1,44 @@
 import { cva, VariantProps } from "class-variance-authority";
-import { cn } from "../utils/tw-merge";
 
 export const ChipVariants = cva(
   `
-  flex-shrink-0 text-center text-gray-50 font-semibold rounded-[2.2rem]
+  flex-shrink-0 text-center font-semibold rounded-[2.2rem]
   `,
   {
     variants: {
-      variant: {
-        default: "bg-[#dfc7c7] ",
-        hoverRed: "bg-[#892122]",
-        red: "bg-white text-[#892122]",
+      color: {
+        default: "bg-[#dfc7c7]",
+        selected: "bg-[#892122]",
+        red: "bg-white",
       },
-      // FIXME: sm,md,lg,xlg 임시 값
+      // FIXME: sm,lg,xl 임시 값
       size: {
-        default: "p-[0.6rem] px-7 text-2xl",
-        sm: "p-[0.6rem] px-8 text-xl",
-        md: "p-[0.7rem] px-9 text-3xl",
+        sm: "p-[0.6rem] px-7 text-lg",
+        md: "p-[0.6rem] px-7 text-2xl",
         lg: "p-[0.8rem] px-10 text-4xl",
-        xlg: "p-[0.9rem] px-11 text-5xl",
+        xl: "p-[0.9rem] px-11 text-5xl",
       },
     },
     defaultVariants: {
-      variant: "default",
-      size: "default",
+      color: "default",
+      size: "md",
     },
+    compoundVariants: [
+      {
+        color: ["default", "selected"],
+        className: "text-white",
+      },
+      {
+        color: "red",
+        className: "text-[#892122]",
+      },
+    ],
   }
 );
 
 interface ChipProps extends VariantProps<typeof ChipVariants> {
   text: string;
-  hasLine?: boolean;
 }
-export const Chip = ({ variant, size, text, hasLine }: ChipProps) => {
-  return (
-    <div className="flex w-full items-center justify-center">
-      {hasLine && <div className="h-[2px] flex-1 rounded-lg bg-[#dfc7c7]" />}
-      <h1 className={cn(ChipVariants({ variant, size }))}>{text}</h1>
-      {hasLine && <div className="h-[2px] flex-1 rounded-lg bg-[#dfc7c7]" />}
-    </div>
-  );
+export const Chip = ({ color, size, text }: ChipProps) => {
+  return <div className={ChipVariants({ color, size })}>{text}</div>;
 };
