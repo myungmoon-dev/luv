ef3c717ca9b57f6df106fab9fd798ad9f013ee3a
NiNinanana <yeen00123@gmail.com>
2024-02-27 18:01:48 +0900

    feat: 성도의 교제 상세 페이지 경로 추가

---
+++ apps/client/src/pages/fellowship/member-news.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import Layout from "@/components/layout";
+
+const FellowshipMemberNewsPage = () => {
+  return (
+    <Layout
+      title="교우소식"
+      bannerDescription="교회여 일어나 세상으로 흘러가라!"
+      bannerImage="/images/balance.jpg"
+      innerMenus={[
+        { label: "명문소식", path: "/fellowship/news" },
+        { label: "교우소식", path: "/fellowship/member-news" },
+        { label: "명문영상", path: "/fellowship/videos" },
+        { label: "주보", path: "/fellowship/bulletins" },
+        { label: "교회 앨범", path: "/fellowship/photos" },
+        { label: "새가족 앨범", path: "/fellowship/new-photos" },
+        { label: "선교지 소식", path: "/fellowship/mission-news" },
+        { label: "남전도회", path: "/fellowship/mens" },
+        { label: "당회소식", path: "/fellowship/session-news" },
+        { label: "자료함", path: "/fellowship/resources" },
+      ]}
+    >
+      <div>FellowshipMemberNewsPage</div>
+    </Layout>
+  );
+};
+
+export default FellowshipMemberNewsPage;
