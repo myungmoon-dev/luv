0d975fa30ad969a0bb0513a5e3d1e36297d347d1
NiNinanana <yeen00123@gmail.com>
2024-02-27 17:51:27 +0900

    feat: 교육부서 상세 페이지 경로 추가

---
+++ apps/client/src/pages/education/high.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Layout from "@/components/layout";
+
+const EducationHighPage = () => {
+  return (
+    <Layout
+      title="고등부"
+      bannerDescription="교회여 일어나 세상으로 흘러가라!"
+      bannerImage="/images/balance.jpg"
+      innerMenus={[
+        { label: "영아부", path: "/education/infants" },
+        { label: "유치부", path: "/education/toddlers" },
+        { label: "유년부", path: "/education/primary" },
+        { label: "초등부", path: "/education/elementary" },
+        { label: "중등부", path: "/education/middle" },
+        { label: "고등부", path: "/education/high" },
+        { label: "2청년", path: "/education/young-adults2" },
+        { label: "1청년", path: "/education/young-adults1" },
+      ]}
+    >
+      <div>EducationHighPage</div>
+    </Layout>
+  );
+};
+
+export default EducationHighPage;
