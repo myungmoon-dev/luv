c877fa745abd3c96c648018cbb18268f3c795bef
wannabeing <wannabeing@naver.com>
2024-03-08 02:41:30 +0900

    chore: 메인 명문앨범&오시는길 수정 및 이미지추가

--- apps/client/src/components/home/section/album.tsx
+++ apps/client/src/components/home/section/album.tsx
@@ -11,19 +11,19 @@ const AlbumSection = () => {
         <div className="flex flex-col items-center gap-4">
           <p>주일 스케치</p>
           <Card className="relative h-[180px] w-full lg:h-[235px]">
-            <Image src="/images/some.jpg" alt="sketch_1" fill className="rounded-lg object-cover" />
+            <Image src="/images/sketch_1.jpg" alt="sketch_1" fill className="rounded-lg object-cover" />
           </Card>
           <Card className="relative h-[180px] w-full lg:h-[235px]">
-            <Image src="/images/some.jpg" alt="sketch_1" fill className="rounded-lg object-cover" />
+            <Image src="/images/sketch_2.jpg" alt="sketch_2" fill className="rounded-lg object-cover" />
           </Card>
         </div>
         <div className="flex flex-col items-center gap-4">
           <p>다음 세대</p>
           <Card className="relative h-[180px] w-full lg:h-[235px]">
-            <Image src="/images/some.jpg" alt="sketch_1" fill className="rounded-lg object-cover" />
+            <Image src="/images/sketch_3.jpg" alt="sketch_3" fill className="rounded-lg object-cover" />
           </Card>
           <Card className="relative h-[180px] w-full lg:h-[235px]">
-            <Image src="/images/some.jpg" alt="sketch_1" fill className="rounded-lg object-cover" />
+            <Image src="/images/sketch_4.jpg" alt="sketch_4" fill className="rounded-lg object-cover" />
           </Card>
         </div>
       </div>
