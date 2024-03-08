2e446527b507d2369bd18fdfd18c80b53fe0ed0c
wannabeing <wannabeing@naver.com>
2024-03-08 21:38:05 +0900

    chore: 명문앨범&오시는길 수정 및 이미지추가

--- apps/client/src/components/home/section/map.tsx
+++ apps/client/src/components/home/section/map.tsx
@@ -9,14 +9,18 @@ const MapSection = () => {
       <div className="grid grid-cols-2 gap-3">
         <div className="flex flex-col items-center gap-5">
           <Chip text="독산동 비전채플(평일)" size="sm" />
-          <p className="text-sm text-gray-400">서울특별시 금천구 남부순환로 1406</p>
+          <p className="h-10 text-sm text-gray-400">서울특별시 금천구 남부순환로 1406</p>
           <Card className="relative h-[230px] w-full">
             <Image src="/images/some.jpg" alt="sketch_1" fill className="rounded-lg object-cover" />
           </Card>
         </div>
         <div className="flex flex-col items-center gap-5">
           <Chip text="서울여상 사랑채플(주일)" size="sm" />
-          <p className="text-sm text-gray-400">서울 관악구 관악로 85 (서울여자상업고등학교 체육관 건물 3층)</p>
+          <p className="h-10 text-center text-sm text-gray-400">
+            서울 관악구 관악로 85
+            <br />
+            (서울여자상업고등학교 체육관 건물 3층)
+          </p>
           <Card className="relative h-[230px] w-full">
             <Image src="/images/some.jpg" alt="sketch_1" fill className="rounded-lg object-cover" />
           </Card>
