e13360f62dc3ace96598d69e5f25238347e7e2dd
wannabeing <wannabeing@naver.com>
2024-03-10 01:40:08 +0900

    chore: 헤더 컴포넌트에 메인페이지 리다이렉트 수정

--- packages/ui/src/Header/index.tsx
+++ packages/ui/src/Header/index.tsx
@@ -36,36 +36,38 @@ export const Header = ({ push }: IHeaderProps) => {
   return (
     <header
       className={cn(
-        "ui-z-10 ui-sticky -ui-top-8 ui-pt-8",
-        isScrolled ? "ui-px-0" : "ui-px-10"
+        "ui-z-10",
+        isScrolled
+          ? "ui-sticky ui-top-0 ui-px-0"
+          : "ui-w-full ui-absolute -ui-top-0 ui-pt-8 ui-px-4 sm:ui-px-10"
       )}
     >
       <div
         className={cn(
           "ui-h-[60px] ui-bg-white ui-w-full ui-shadow-lg",
-          isScrolled ? "md:ui-px-20" : "ui-px-8 md:ui-px-10 ui-rounded-full"
+          isScrolled ? "md:ui-px-20" : "ui-px-4 md:ui-px-10 ui-rounded-full"
         )}
       >
         <div
           className={cn(
-            " ui-flex ui-justify-between ui-items-center ui-w-full ui-h-full"
+            " ui-flex ui-justify-between ui-items-center ui-w-full ui-h-full ui-px-3 sm:ui-px-0"
           )}
         >
           <img
+            className="ui-cursor-pointer"
             onClick={() => push("/")}
             src="/images/Logo.png"
             width={100}
             height={50}
             alt="myungmoon"
-            className="ui-cursor-pointer"
           />
           <nav className="ui-flex ui-items-center ui-gap-5 lg:ui-gap-7">
-            <div className="ui-flex ui-gap-3 lg:ui-gap-5">
+            <div className="ui-hidden ui-gap-3 lg:ui-gap-5 sm:ui-flex">
               {menus.map((menu) => (
                 <button
                   onClick={() => push(menu.path)}
                   key={menu.path}
-                  className="ui-text-pink-200 ui-font-semibold text-sm"
+                  className="ui-text-pink-200 ui-font-semibold ui-text-sm"
                 >
                   {menu.label}
                 </button>
