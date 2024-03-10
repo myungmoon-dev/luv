586b7e7b241bfee2b8442c344ed31f0a005bb26e
wannabeing <wannabeing@naver.com>
2024-03-11 00:14:48 +0900

    feat: 메인페이지에 firebase 연결 및 API 적용

--- apps/client/src/components/home/section/words.tsx
+++ apps/client/src/components/home/section/words.tsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card } from "ui";
 import Section from ".";
 import YoutubeVideo from "@/components/youtubeVideo";
 
+interface IFetcher {
+  url: string;
+
+  method: string;
+}
+
+const fetcher = ({ method, url }: IFetcher) =>
+  fetch(url, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+
 const WordsSection = () => {
+  const [livelink, setLivelink] = useState("");
+
+  const fetchLivelink = async () => {
+    try {
+      const result = await fetcher({ url: "/api/youtube", method: "GET" });
+      setLivelink(result.livelink);
+
+      console.log(livelink);
+    } catch (error) {
+      // FIXME: Handle errors here
+    }
+  };
+
+  useEffect(() => {
+    fetchLivelink();
+  }, []);
   return (
     <Section title="생명의 말씀">
       <div className="flex flex-col gap-5 sm:flex-row">
         <Card className="relative h-[300px] w-[100%] sm:w-[70%] lg:h-[450px]">
-          <YoutubeVideo videoId="SjRpgTxF9s0?si=vFiI9rWUonj0UTc8" />
+          <YoutubeVideo videoId={livelink} />
         </Card>
         <Card className="relative h-[300px] w-[100%] sm:w-[30%] lg:h-[450px]">
           <YoutubeVideo videoId="omBT6ttv-Us?si=K3uAXM8MV3ahPf3F" />
