6131b060ee2f8bcb3195b32d2168ef3f50a5860f
wannabeing <wannabeing@naver.com>
2024-03-10 19:48:59 +0900

    refactor: 오타수정 및 카멜케이스 적용

--- apps/client/src/components/home/section/map.tsx
+++ apps/client/src/components/home/section/map.tsx
@@ -10,12 +10,12 @@ const MapSection = () => {
       <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 sm:gap-3">
         <div className="flex flex-col items-center gap-5">
           <Chip text="독산동 비전채플(평일)" size="sm" />
-          <p className="m:h-10 h-7 text-center text-sm text-gray-400">서울특별시 금천구 남부순환로 1406</p>
+          <p className="h-7 text-center text-sm text-gray-400 md:h-10">서울특별시 금천구 남부순환로 1406</p>
           <KakaoMap address="서울특별시 금천구 남부순환로 1406" />
         </div>
         <div className="flex flex-col items-center gap-5">
           <Chip text="서울여상 사랑채플(주일)" size="sm" />
-          <p className="m:h-10 h-7 text-center text-sm text-gray-400">
+          <p className="h-7 text-center text-sm text-gray-400 md:h-10">
             서울특별시 관악구 관악로 85
             <br />
             (서울여자상업고등학교 체육관 건물 3층)
