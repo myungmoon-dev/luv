3345e155631627af1801ae1c24d73317aebdd50c
NiNinanana <yeen00123@gmail.com>
2024-03-10 22:08:39 +0900

    refactor: banner chip layout 수정

--- packages/ui/src/Banner/index.tsx
+++ packages/ui/src/Banner/index.tsx
@@ -11,16 +11,7 @@ interface BannerProps {
   onClickChip: (path: string) => void;
 }
 
-export const Banner = ({
-  description,
-  image,
-  title,
-  innerMenus,
-  video,
-  pathname,
-  onClickChip,
-  push,
-}: BannerProps) => {
+export const Banner = ({ description, image, title, innerMenus, video, pathname, onClickChip, push }: BannerProps) => {
   return (
     <div className="ui-w-full ui-relative">
       {image ? (
@@ -37,14 +28,13 @@ export const Banner = ({
           className="ui-w-full ui-h-[550px] sm:ui-h-[750px] ui-object-cover"
         />
       )}
-      <div className="ui-absolute ui-flex ui-flex-col ui-gap-5 ui-items-center ui-px-24 ui-w-full ui-bottom-[100px] ui-left-1/2 -ui-translate-x-1/2 ui-text-white">
+      <div className="ui-absolute ui-flex ui-flex-col ui-gap-5 ui-items-center ui-px-8 md:ui-px-16 xl:ui-px-24 ui-w-full ui-bottom-[100px] ui-left-1/2 -ui-translate-x-1/2 ui-text-white">
         <div className="ui-flex ui-flex-col ui-gap-2 ui-items-center">
           <h1 className="ui-text-2xl ui-font-bold">{title}</h1>
           <p className="ui-font-thin">{description}</p>
         </div>
         {title && <hr className="ui-w-full" />}
-        {/* FIXME: Chip 컴포넌트 만들어지면 적용 */}
-        <div className="ui-place-items-center ui-grid ui-grid-cols-1 sm:ui-grid-cols-3 lg:ui-grid-cols-6 ui-gap-4 ui-place-content-center">
+        <div className="ui-gap-4 ui-flex ui-flex-wrap ui-justify-center">
           {innerMenus?.map((menu) => (
             <Chip
               onClick={() => onClickChip(menu.path)}
