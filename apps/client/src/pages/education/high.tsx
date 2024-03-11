b606e6fc5a000d4a1d377607e7c939f8113be3f6
wannabeing <wannabeing@naver.com>
2024-03-11 22:54:54 +0900

    chore: 다음세대 중고등부페이지 배너이미지 수정

--- apps/client/src/pages/education/high.tsx
+++ apps/client/src/pages/education/high.tsx
@@ -10,7 +10,7 @@ const EducationHighPage = () => {
       pageTitle="고등부"
       title="고등부"
       bannerDescription="교회여 일어나 세상으로 흘러가라!"
-      bannerImage="/images/next_g.jpeg"
+      bannerImage="/images/edu_high.jpeg"
       innerMenus={educationInnerMenus}
     >
       <NotPrepared />
