3510ee6f812671b8f9971b3db766c53177e1ea1c
ChoiHyuk <wannabeing@naver.com>
2024-03-11 17:55:11 +0900

    feat/admin: 관리자페이지 react-query 설치 및 shorts 추가

--- apps/admin/src/pages/api/youtube/index.ts
+++ apps/admin/src/pages/api/youtube/index.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getLiveLink, createLiveLink } from "firebase";
+import { getLiveLink, createLiveLink, getShorts, createShorts } from "firebase";
 
 export default async function handler(
   req: NextApiRequest,
@@ -7,20 +7,43 @@ export default async function handler(
 ) {
   const { method, body } = req;
   const videoId = body.url as string;
+  const videoType = body.videotype as string;
 
+  // FIXME: return Value, videoType
   switch (method) {
     case "GET":
-      const snapshot = await getLiveLink();
-      const livelink = snapshot.docs.map((doc) => doc.data().videoId);
+      switch (videoType) {
+        case "SHORTS":
+          const shortsSnapshot = await getShorts();
+          const shorts = shortsSnapshot.docs.map((doc) => doc.data().videoId);
 
-      return res.status(200).json({
-        livelink,
-      });
+          return res.status(200).json({
+            shorts,
+          });
+        case "LIVELINK":
+          const livelinkSnapshot = await getLiveLink();
+          const livelink = livelinkSnapshot.docs.map(
+            (doc) => doc.data().videoId
+          );
+
+          return res.status(200).json({
+            livelink,
+          });
+      }
     case "POST":
-      await createLiveLink({ videoId });
-      return res.status(200).json({
-        success: true,
-      });
+      switch (videoType) {
+        case "SHORTS":
+          await createShorts({ videoId });
+          return res.status(200).json({
+            success: true,
+          });
+        case "LIVELINK":
+          await createLiveLink({ videoId });
+          return res.status(200).json({
+            success: true,
+          });
+      }
+
     default:
       res.setHeader("Allow", ["GET", "POST"]);
       res.status(405).end(`Method ${method} Not Allowed`);
