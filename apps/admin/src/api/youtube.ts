a59770bd1ecff45dc881a57edff163cd7f2c685a
ChoiHyuk <wannabeing@naver.com>
2024-03-13 17:33:10 +0900

    refactor: youtube query 수정

---
+++ apps/admin/src/api/youtube.ts
@@ -0,0 +1,35 @@
+import { api } from ".";
+
+interface IPostFetcherProps {
+  vid: string;
+  type: YoutubeType;
+}
+// FIXME: types/youtube/response.ts로 빼야함
+interface IGetYoutubeResponse {
+  youtubeLink: string[];
+}
+interface IPostYoutubeResponse {
+  result: string;
+}
+
+export const fetcherGET = async (type: YoutubeType) => {
+  const { data } = await api.get<IGetYoutubeResponse>("/api/youtube", {
+    params: { type },
+  });
+  return data;
+};
+
+export const fetcherPOST = async ({ vid, type }: IPostFetcherProps) => {
+  const { data } = await api.post<IPostYoutubeResponse>(
+    "/api/youtube",
+    {
+      vid,
+    },
+    {
+      params: {
+        type,
+      },
+    }
+  );
+  return data;
+};
---
+++ apps/admin/src/query/youtube/index.ts
@@ -0,0 +1,32 @@
+import { useMutation, useQuery, useQueryClient } from "react-query";
+
+import { fetcherGET, fetcherPOST } from "@/api/youtube";
+import youtubeKeys from "./keys";
+
+interface IMutationProps {
+  vid: string;
+}
+
+export const useGetYoutubeLink = (type: YoutubeType) => {
+  const queryKey = youtubeKeys[type]();
+  
+  return useQuery({
+    queryKey,
+    queryFn: async () => await fetcherGET(type),
+    select: (response) => response.youtubeLink[0],
+  });
+};
+
+export const usePostYoutubeLink = (type: YoutubeType) => {
+  const queryClient = useQueryClient();
+  const queryKey = youtubeKeys[type]();
+
+  return useMutation({
+    mutationFn: async ({ vid }: IMutationProps) => {
+      const response = await fetcherPOST({ vid, type });
+      return response.result === "success";
+    },
+    onSuccess: () => queryClient.invalidateQueries(queryKey),
+    onError: (error) => console.error(error),
+  });
+};
