53c9c5bcaeeda17ffef709005d15ed0bcb3b8ff4
NiNinanana <yeen00123@gmail.com>
2024-03-13 16:41:00 +0900

    feat: bulletin 메인 페이지에 연결

--- apps/client/src/components/home/section/news.tsx
+++ apps/client/src/components/home/section/news.tsx
@@ -2,37 +2,33 @@ import React from "react";
 import { Card, cn } from "ui";
 
 import Section from ".";
-
-const MOCK_WEEKLY_NEWS = [
-  { title: "2월 첫째 주 주보", date: "2024-02-04" },
-  { title: "1월 넷째 주 주보", date: "2024-01-28" },
-  { title: "1월 셋째 주 주보", date: "2024-01-21" },
-  { title: "1월 둘째 주 주보", date: "2024-01-14" },
-  { title: "1월 첫째 주 주보", date: "2024-01-07" },
-];
+import { useGetBulletins } from "@/query/bulletin";
 
 const NewsSection = () => {
+  const { data } = useGetBulletins();
+
   return (
     <Section title="교회 소식">
       <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
         <div>
-          {MOCK_WEEKLY_NEWS.map((news, idx) => (
-            <div className="mt-4 flex flex-col gap-1" key={news.date}>
-              <div className="flex justify-around">
-                <span
-                  className={cn(
-                    "flex h-5 w-5 items-center justify-center bg-[#dfc7c7] text-center font-bold text-white",
-                    idx === 0 ? "visible" : "invisible",
-                  )}
-                >
-                  N
-                </span>
-                <p>{news.title}</p>
-                <p className="text-sm text-gray-400">{news.date}</p>
+          {data &&
+            data.bulletins.map((bulletin, idx) => (
+              <div className="mt-4 flex flex-col gap-1" key={bulletin.id}>
+                <div className="flex justify-around">
+                  <span
+                    className={cn(
+                      "flex h-5 w-5 items-center justify-center bg-[#dfc7c7] text-center font-bold text-white",
+                      idx === 0 ? "visible" : "invisible",
+                    )}
+                  >
+                    N
+                  </span>
+                  <p>{bulletin.title}</p>
+                  <p className="text-sm text-gray-400">{bulletin.date}</p>
+                </div>
+                <hr className="border-t-2 border-gray-100" />
               </div>
-              <hr className="border-t-2 border-gray-100" />
-            </div>
-          ))}
+            ))}
         </div>
         <Card className="flex h-[300px] items-center justify-center bg-gray-100 sm:h-auto">주보 미리보기</Card>
       </div>
