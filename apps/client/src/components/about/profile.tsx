d3610fa71e830c9a70867d678deef5786184d250
ChoiHyuk <wannabeing@naver.com>
2024-03-15 17:58:06 +0900

    refacotr: Profile 컴포넌트 수정

--- apps/client/src/components/about/profile.tsx
+++ apps/client/src/components/about/profile.tsx
@@ -2,19 +2,32 @@ import Image from "next/image";
 import React from "react";
 
 interface IProfileProps {
+  index: number;
   name: string;
   description: string;
   image: string;
-  alt?: string;
+  alt: string;
+  className?: string;
+  position: string;
 }
 
-const Profile = ({ description, image, name, alt = "profile" }: IProfileProps) => {
+const Profile = ({ description, image, name, alt, className, position, index }: IProfileProps) => {
   return (
-    <div className="flex items-center gap-5">
-      <Image src={image} alt={alt} width={120} height={160} />
-      <div className="flex flex-col gap-2">
-        <h2 className="text-xl">{name}</h2>
-        <p>{description}</p>
+    <div className={`flex items-start gap-20 border-b-[1px] border-gray-300 pb-5 ${className}`}>
+      <div className="relative flex h-[250px] w-[200px] items-center justify-center overflow-hidden rounded-xl shadow-2xl ">
+        <Image src={image} alt={alt} fill className="object-cover" />
+      </div>
+      <div className="flex flex-col gap-5">
+        <h2 className="text-2xl font-bold text-[#892122]">{name}</h2>
+        <div className="flex flex-col gap-5">
+          <p className="font-semibold">{position}</p>
+          {index > 1 ? (
+            <div className="flex items-center gap-2">
+              <p className="rounded-md bg-[#892122] px-2 py-1 font-bold text-white">부임</p>
+              <p className="text-gray-800">{description}</p>
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   );
