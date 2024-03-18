f48f515ac3e00e17962886466454216a4dffda0e
ChoiHyuk <wannabeing@naver.com>
2024-03-18 14:01:59 +0900

    chore: SectionHeader 컴포넌트 색상 추가

--- packages/ui/src/SectionHeader/index.tsx
+++ packages/ui/src/SectionHeader/index.tsx
@@ -4,15 +4,27 @@ import { cn } from "../utils/twMerge";
 interface ISectionHeaderProps {
   text: string;
   hasLine?: boolean;
-  color?: "pink";
+  color?: "pink" | "red";
   size?: "sm" | "md" | "lg" | "xl";
   selected?: boolean;
   className?: string;
 }
 
-export const SectionHeader = ({ text, hasLine, color, size, selected, className }: ISectionHeaderProps) => {
+export const SectionHeader = ({
+  text,
+  hasLine,
+  color,
+  size,
+  selected,
+  className,
+}: ISectionHeaderProps) => {
   return (
-    <div className={cn("ui-flex ui-w-full ui-items-center ui-justify-center", className)}>
+    <div
+      className={cn(
+        "ui-flex ui-w-full ui-items-center ui-justify-center",
+        className
+      )}
+    >
       {hasLine && <Line />}
       <Chip text={text} color={color} size={size} selected={selected} />
       {hasLine && <Line />}
