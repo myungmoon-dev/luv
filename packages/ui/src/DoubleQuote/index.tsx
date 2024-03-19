6aa63e4080f499e5ac1cf24c9b6ee916606bcadd
ChoiHyuk <wannabeing@naver.com>
2024-03-19 10:42:04 +0900

    Feat/about introduce (#44)
    * feat: 쌍따옴표 컴포넌트 생성
    
    * refactor: 교회소개 페이지 수정
    
    * chore: 쌍따옴표 UI 컴포넌트 export 수정

---
+++ packages/ui/src/DoubleQuote/index.tsx
@@ -0,0 +1,43 @@
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "../utils/twMerge";
+import { HTMLAttributes } from "react";
+
+export const DoubleQuoteVariants = cva(`ui-font-bold`, {
+  variants: {
+    color: {
+      pink: "",
+    },
+    size: {
+      sm: "ui-text-2xl sm:ui-text-4xl md:ui-text-6xl",
+      md: "ui-text-4xl sm:ui-text-6xl md:ui-text-8xl",
+      lg: "ui-text-5xl sm:ui-text-7xl md:ui-text-9xl",
+      xl: "ui-text-6xl sm:ui-text-7xl md:ui-text-9xl",
+    },
+  },
+  defaultVariants: {
+    color: "pink",
+    size: "md",
+  },
+  compoundVariants: [
+    {
+      color: "pink",
+      size: "md",
+      className: "ui-text-[#c8b3b3]",
+    },
+  ],
+});
+
+interface IDoubleQuoteProps
+  extends VariantProps<typeof DoubleQuoteVariants>,
+    Omit<HTMLAttributes<HTMLDivElement>, "color"> {}
+
+export const DoubleQuote = ({
+  color,
+  size,
+  className,
+  ...props
+}: IDoubleQuoteProps) => {
+  return (
+    <div className={cn(DoubleQuoteVariants({ color, size }), className)}>"</div>
+  );
+};
