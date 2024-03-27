9a1889fd9cdc848f70e943276dc0a6f1b1e12841
ChoiHyuk <wannabeing@naver.com>
2024-03-27 17:03:55 +0900

    refactor: 메인 iframe 유튜브 컴포넌트 Spinner 적용

--- apps/client/src/components/home/section/words.tsx
+++ apps/client/src/components/home/section/words.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Card } from "ui";
 import Section from ".";
-import YoutubeVideo from "@/components/youtubeVideo";
 import { useGetYoutubeLink } from "@/query/youtube";
+import YoutubeVideo from "@/components/youtubeVideo";
 
 const WordsSection = () => {
   const { data: youtubeLink } = useGetYoutubeLink("youtube");
   const { data: shortsLink } = useGetYoutubeLink("shorts");
   const { data: liveLink } = useGetYoutubeLink("live");
 
-  return youtubeLink && shortsLink ? (
+  return (
     <Section title="생명의 말씀">
       <div className="flex flex-col gap-5 sm:flex-row">
         <Card className="relative h-[300px] w-[100%] sm:w-[70%] lg:h-[450px]">
@@ -20,7 +20,7 @@ const WordsSection = () => {
         </Card>
       </div>
     </Section>
-  ) : null;
+  );
 };
 
 export default WordsSection;
--- apps/client/src/components/youtubeVideo.tsx
+++ apps/client/src/components/youtubeVideo.tsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Spinner } from "ui";
 
 type YoutubeVideoProps = {
-  videoId: string;
+  videoId: string | undefined;
 };
 
 const YoutubeVideo = ({ videoId }: YoutubeVideoProps) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
   return (
-    <iframe className="h-full w-full rounded-lg" src={`https://www.youtube.com/embed/${videoId}`} allowFullScreen />
+    <>
+      {isLoading ? (
+        <div className="absolute inset-0 flex items-center justify-center bg-transparent">
+          <Spinner loading={isLoading} />
+        </div>
+      ) : (
+        <iframe
+          className="h-full w-full rounded-lg"
+          src={`https://www.youtube.com/embed/${videoId}`}
+          allowFullScreen
+          loading="lazy"
+          onLoad={() => setIsLoading(false)}
+        />
+      )}
+    </>
   );
 };
 
