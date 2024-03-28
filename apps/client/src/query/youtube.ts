c70e79fd99ad4a4b31fbc1b1d961e7061f234a6e
ChoiHyuk <wannabeing@naver.com>
2024-03-28 14:37:25 +0900

    chore: tanstack/react-query로 변경

--- apps/client/src/query/youtube.ts
+++ apps/client/src/query/youtube.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 import youtubeKeys from "./youtube/keys";
 import { YoutubeType, getYoutubeLink } from "@/api/youtube";
 
