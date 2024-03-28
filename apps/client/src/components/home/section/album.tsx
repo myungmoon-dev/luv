74ceb9cdd4c92ae3eabf52e15c4cc44ab562b174
ChoiHyuk <wannabeing@naver.com>
2024-03-28 14:43:48 +0900

    Chore/qa 0327 (#48)
    * feat: 404 컴포넌트 생성
    
    * chore: album 컴포넌트 이미지 lazyloading 설정
    
    * refactor: sharp 패키지 버전 다운 재설치
    
    * chore: 404 컴포넌트명 수정

--- apps/client/src/components/home/section/album.tsx
+++ apps/client/src/components/home/section/album.tsx
@@ -11,19 +11,19 @@ const AlbumSection = () => {
         <div className="flex flex-col items-center gap-4">
           <p className="font-bold">주일 스케치</p>
           <Card className="relative h-[250px] w-full">
-            <Image src="/images/sketch-1.jpg" alt="sketch1" fill className="rounded-lg object-cover" />
+            <Image src="/images/sketch-1.jpg" alt="sketch1" fill className="rounded-lg object-cover" loading="lazy" />
           </Card>
           <Card className="relative h-[250px] w-full">
-            <Image src="/images/sketch-2.jpg" alt="sketch2" fill className="rounded-lg object-cover" />
+            <Image src="/images/sketch-2.jpg" alt="sketch2" fill className="rounded-lg object-cover" loading="lazy" />
           </Card>
         </div>
         <div className="flex flex-col items-center gap-4">
           <p className="font-bold">다음 세대</p>
           <Card className="relative h-[250px] w-full">
-            <Image src="/images/sketch-3.jpg" alt="sketch3" fill className="rounded-lg object-cover" />
+            <Image src="/images/sketch-3.jpg" alt="sketch3" fill className="rounded-lg object-cover" loading="lazy" />
           </Card>
           <Card className="relative h-[250px] w-full">
-            <Image src="/images/sketch-4.jpg" alt="sketch4" fill className="rounded-lg object-cover" />
+            <Image src="/images/sketch-4.jpg" alt="sketch4" fill className="rounded-lg object-cover" loading="lazy" />
           </Card>
         </div>
       </div>
