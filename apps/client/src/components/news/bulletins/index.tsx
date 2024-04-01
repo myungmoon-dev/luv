1fb74222631b4f312b9f6db35fed80f1cd003ee3
NiNinanana <yeen00123@gmail.com>
2024-04-01 20:15:34 +0900

    feat: 주보 페이지 api 적용

--- apps/client/src/components/news/bulletins/index.tsx
+++ apps/client/src/components/news/bulletins/index.tsx
@@ -1,16 +1,32 @@
+import { useGetBulletins } from "@/query/bulletin";
 import React from "react";
-import { ITableData, Table } from "ui";
-
-const MOCK_TABLE_DATA: ITableData[] = [
-  { date: "2024. 04. 03", id: "3", title: "2024년 3월 5번째주", viewCount: 5, writer: "admin" },
-  { date: "2024. 04. 03", id: "2", title: "2024년 3월 4번째주", viewCount: 3, writer: "admin" },
-  { date: "2024. 04. 03", id: "1", title: "2024년 3월 3번째주", viewCount: 10, writer: "admin" },
-];
+import { Table, Pagination } from "ui";
+import BulletinModal from "./Modal";
+import useModalStore from "@/store/modal";
 
 const Bulletins = () => {
+  const { data } = useGetBulletins();
+  const open = useModalStore((state) => state.open);
+
+  const handleBulletinClick = (id: string) => {
+    open(<BulletinModal selectedBulletinId={id} />);
+  };
+
+  if (!data) return <p>loading...</p>;
+
   return (
-    <div>
-      <Table data={MOCK_TABLE_DATA} />
+    <div className="flex flex-col gap-7">
+      <Table
+        data={data.bulletins.map((bulletin) => ({
+          id: bulletin.id,
+          date: bulletin.createdAt,
+          title: bulletin.title,
+          writer: "관리자",
+        }))}
+        onClickRow={handleBulletinClick}
+      />
+      {/* FIXME: api에서 페이지네이션 정보 보내주도록 수정 */}
+      <Pagination currentPage={1} onSetPage={() => {}} totalQuantity={data.bulletins.length} />
     </div>
   );
 };
