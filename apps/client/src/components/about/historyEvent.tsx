61522fb526a0789be4a2a3dd47fb10b32c1af985
ChoiHyuk <wannabeing@naver.com>
2024-04-03 15:09:18 +0900

    feat: historyEvent 컴포넌트 생성

---
+++ apps/client/src/components/about/historyEvent.tsx
@@ -0,0 +1,41 @@
+import { IHistoryEvent } from "@/constants/innerMenus/types";
+import { cn } from "ui";
+
+interface IHistoryEventProps {
+  event: IHistoryEvent;
+  index: number;
+  length: number;
+}
+
+const HistoryEvent = ({ index, length, event }: IHistoryEventProps) => {
+  return (
+    <div className="relative grid grid-cols-2 place-items-center gap-16">
+      <p
+        className={cn(
+          "justify-self-end text-sm font-semibold text-pink-100",
+          index === 0 && "self-start",
+          length - 1 === index && "self-end",
+        )}
+      >
+        {event.date}
+      </p>
+      <p
+        className={cn(
+          "max-w-lg justify-self-start text-sm",
+          index === 0 && "self-start",
+          length - 1 === index && "self-end",
+        )}
+      >
+        {event.description}
+      </p>
+      <div
+        className={cn(
+          "absolute left-1/2 h-4 w-4 -translate-x-1/2 transform rounded-full bg-pink-100",
+          index === 0 && "top-0",
+        )}
+      />
+    </div>
+  );
+};
+
+export default HistoryEvent;
