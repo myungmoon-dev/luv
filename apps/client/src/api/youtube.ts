bddc0d2fa7f80e1fa9b8f03b84eb5e8b7720ddc4
macmini <wannabeing@naver.com>
2024-04-05 21:08:07 +0900

    chore: Request 타입 생성

--- apps/client/src/api/youtube.ts
+++ apps/client/src/api/youtube.ts
@@ -1,12 +1,12 @@
 import { api } from ".";
 import { IGetYoutubeResponse, YoutubeType } from "type";
 
-export interface IGetYoutubeListProps {
+export interface IGetYoutbeRequest {
   videoType: YoutubeType;
   videoCount?: number;
 }
 
-export const getYoutubeList = async ({ videoType, videoCount }: IGetYoutubeListProps) => {
+export const getYoutubeList = async ({ videoType, videoCount }: IGetYoutbeRequest) => {
   const { data } = await api.get<IGetYoutubeResponse>("/api/youtube", {
     params: { type: videoType, count: videoCount },
   });
