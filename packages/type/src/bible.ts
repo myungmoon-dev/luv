d4a2f11df2d2a27ed1e6ad3669f1b39e0b62e0f8
NiNinanana <yeen00123@gmail.com>
2024-04-08 13:09:37 +0900

    refactor: bible에 links 추가

--- packages/type/src/bible.ts
+++ packages/type/src/bible.ts
@@ -1,5 +1,9 @@
 import { IBoardBase, IBoardFormBase } from "./common";
 
-export interface IBible extends IBoardBase {}
+export interface IBible extends IBoardBase {
+  links: string[];
+}
 
-export interface IBibleForm extends IBoardFormBase {}
+export interface IBibleForm extends IBoardFormBase {
+  links: string[];
+}
