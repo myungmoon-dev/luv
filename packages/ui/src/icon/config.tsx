acdd6c3769c5b70e523d6306cbfd268d5b38fe7e
ChoiHyuk <wannabeing@naver.com>
2024-04-08 17:53:37 +0900

    feat: Bible 아이콘 추가

--- packages/ui/src/icon/config.tsx
+++ packages/ui/src/icon/config.tsx
@@ -3,12 +3,13 @@ import CrossIcon from "./assets/cross";
 import YoutubeIcon from "./assets/youtube";
 import OpenBookIcon from "./assets/openBook";
 import HamburgerIcon from "./assets/hamburger";
+import BibleIcon from "./assets/bible";
 
 export type IconCursorType = `ui-cursor-${CursorType}`;
 
 export type IconNameType = keyof typeof icons;
 
-export type IconSizeType = "sm" | "md" | "lg";
+export type IconSizeType = "sm" | "md" | "lg" | "xl" | "2xl";
 
 export interface IIconProps {
   backgroundColor?: string;
@@ -19,6 +20,8 @@ export const widthSize: Record<IconSizeType, `ui-w-[${string}]`> = {
   sm: "ui-w-[16px]",
   md: "ui-w-[20px]",
   lg: "ui-w-[24px]",
+  xl: "ui-w-[50px]",
+  "2xl": "ui-w-[70px]",
 };
 
 export const icons = {
@@ -26,4 +29,5 @@ export const icons = {
   Hamburger: (props: IIconProps) => <HamburgerIcon {...props} />,
   Youtube: (props: IIconProps) => <YoutubeIcon {...props} />,
   OpenBook: (props: IIconProps) => <OpenBookIcon {...props} />,
+  Bible: (props: IIconProps) => <BibleIcon {...props} />,
 };
