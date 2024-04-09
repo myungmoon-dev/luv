e9c8bfd20c172aeb8b2bbc4e041efb24577e91cd
ChoiHyuk <wannabeing@naver.com>
2024-04-09 16:58:52 +0900

    feat: 바로가기 섹션 컴포넌트 추가

--- apps/client/src/components/home/index.tsx
+++ apps/client/src/components/home/index.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-
 import WordsSection from "./section/words";
 import AlbumSection from "./section/album";
 import MapSection from "./section/map";
-import NewsSection from "./section/news";
+import ShortCutsSection from "./section/shortcuts";
 
 const HomePage = () => {
   return (
     <div className="flex flex-col gap-y-10">
+      <ShortCutsSection />
       <WordsSection />
-      <NewsSection />
       <AlbumSection />
       <MapSection />
     </div>
