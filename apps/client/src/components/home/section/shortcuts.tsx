f3cf4b3260035549fb9c32ef04ba0369344aa568
ChoiHyuk <wannabeing@naver.com>
2024-04-11 17:59:10 +0900

    Icon 크기 및 스타일 수정 (#60)

--- apps/client/src/components/home/section/shortcuts.tsx
+++ apps/client/src/components/home/section/shortcuts.tsx
@@ -5,32 +5,32 @@ import SectionCard from "../sectionCard";
 const ShortCutsSection = () => {
   return (
     <Section title="바로가기">
-      <div className="mx-auto my-10 grid max-w-screen-lg grid-cols-2 gap-10 sm:grid-cols-4">
+      <div className="mx-auto my-10 grid max-w-screen-lg grid-cols-2 sm:grid-cols-4">
         <SectionCard
           className="h-[120px] w-[130px] rounded-3xl bg-pink-300 shadow-lg duration-700 hover:bg-pink-200"
           text="설교 영상"
           icon={
-            <Icon name="Youtube" size="2xl" backgroundColor="white" strokeColor="white" cursor="ui-cursor-pointer" />
+            <Icon name="Youtube" size="xl" backgroundColor="white" strokeColor="white" cursor="ui-cursor-pointer" />
           }
           url="sermons/sunday-3"
         />
         <SectionCard
           className="h-[120px] w-[130px] rounded-3xl bg-pink-300 shadow-lg duration-700 hover:bg-pink-200"
           text="주보"
-          icon={<Icon name="Pray" size="2xl" backgroundColor="white" strokeColor="white" cursor="ui-cursor-pointer" />}
+          icon={<Icon name="Pray" size="xl" backgroundColor="white" strokeColor="white" cursor="ui-cursor-pointer" />}
           url="news/bulletins"
         />
         <SectionCard
           className="h-[120px] w-[130px] rounded-3xl bg-pink-300 shadow-lg duration-700 hover:bg-pink-200"
           text="예배 안내"
-          icon={<Icon name="Bell" size="2xl" backgroundColor="white" strokeColor="white" cursor="ui-cursor-pointer" />}
+          icon={<Icon name="Bell" size="xl" backgroundColor="white" strokeColor="white" cursor="ui-cursor-pointer" />}
           url="about/services"
         />
         <SectionCard
           className="h-[120px] w-[130px] rounded-3xl bg-pink-300 shadow-lg duration-700 hover:bg-pink-200"
           text="새가족 안내"
           icon={
-            <Icon name="HandHeart" size="2xl" backgroundColor="white" strokeColor="white" cursor="ui-cursor-pointer" />
+            <Icon name="HandHeart" size="xl" backgroundColor="white" strokeColor="white" cursor="ui-cursor-pointer" />
           }
           url="discipleship/new"
         />
