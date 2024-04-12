75a017b48a85b993b57777e29ab5d8aa75b0a638
ChoiHyuk <wannabeing@naver.com>
2024-04-12 17:34:37 +0900

    chore: 다음세대 세부메뉴 변수 생성

--- apps/client/src/constants/innerMenus/education.ts
+++ apps/client/src/constants/innerMenus/education.ts
@@ -10,3 +10,17 @@ export const educationInnerMenus: IInnerMenu[] = [
   { label: "2청년", path: "/education/youth-adults2" },
   { label: "1청년", path: "/education/youth-adults1" },
 ];
+
+const commonEducationDetailMenus = (basePath: string): IInnerMenu[] => [
+  { label: "부서소개", path: `${basePath}` },
+  { label: "앨범", path: `${basePath}/photos` },
+];
+
+export const educationInfantsMenus: IInnerMenu[] = commonEducationDetailMenus("/education/infants");
+export const educationToddlersMenus: IInnerMenu[] = commonEducationDetailMenus("/education/toddlers");
+export const educationPrimaryMenus: IInnerMenu[] = commonEducationDetailMenus("/education/primary");
+export const educationElementaryMenus: IInnerMenu[] = commonEducationDetailMenus("/education/elementary");
+export const educationMiddleMenus: IInnerMenu[] = commonEducationDetailMenus("/education/middle");
+export const educationHighMenus: IInnerMenu[] = commonEducationDetailMenus("/education/high");
+export const educationYouthAdults2Menus: IInnerMenu[] = commonEducationDetailMenus("/education/youth-adults2");
+export const educationYouthAdults1Menus: IInnerMenu[] = commonEducationDetailMenus("/education/youth-adults1");
