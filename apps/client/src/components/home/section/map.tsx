7a42f7bea4ee64b2a820f3032c3849a7d4311184
NiNinanana <yeen00123@gmail.com>
2024-04-20 18:02:50 +0900

    design: main 수정

--- apps/client/src/components/home/section/map.tsx
+++ apps/client/src/components/home/section/map.tsx
@@ -8,14 +8,14 @@ const MapSection = () => {
     <Section title="오시는 길">
       <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 sm:gap-3">
         <div className="flex flex-col items-center gap-5">
-          <Chip className="duration-500 hover:bg-pink-200" text="독산동 비전채플(평일)" size="sm" selected={true} />
+          <Chip color="blue" className="duration-500" text="독산동 비전채플(평일)" size="sm" selected={true} />
           <p className="h-7 text-center text-sm text-gray-500 md:h-10 lg:text-base">
             서울특별시 금천구 남부순환로 1406
           </p>
           <KakaoMap address="서울특별시 금천구 남부순환로 1406" />
         </div>
         <div className="flex flex-col items-center gap-5">
-          <Chip className="duration-500 hover:bg-pink-200" text="서울여상 사랑채플(주일)" size="sm" selected={true} />
+          <Chip color="blue" className="duration-500" text="서울여상 사랑채플(주일)" size="sm" selected={true} />
           <p className="h-7 text-center text-sm text-gray-500 md:h-10 lg:text-base">
             서울특별시 관악구 관악로 85
             <br />
--- apps/client/src/components/home/section/shortcuts.tsx
+++ apps/client/src/components/home/section/shortcuts.tsx
@@ -7,7 +7,7 @@ const ShortCutsSection = () => {
     <Section title="바로가기">
       <div className="mx-auto my-10 grid max-w-screen-lg grid-cols-2 sm:grid-cols-4">
         <SectionCard
-          className="h-[120px] w-[130px] rounded-3xl bg-pink-300 shadow-lg duration-700 hover:bg-pink-200"
+          className="h-[120px] w-[130px] rounded-3xl bg-blue-400 shadow-lg duration-700"
           text="설교 영상"
           icon={
             <Icon name="Youtube" size="xl" backgroundColor="white" strokeColor="white" cursor="ui-cursor-pointer" />
@@ -15,19 +15,19 @@ const ShortCutsSection = () => {
           url="sermons/sunday-3"
         />
         <SectionCard
-          className="h-[120px] w-[130px] rounded-3xl bg-pink-300 shadow-lg duration-700 hover:bg-pink-200"
+          className="h-[120px] w-[130px] rounded-3xl bg-blue-400 shadow-lg duration-700"
           text="주보"
           icon={<Icon name="Pray" size="xl" backgroundColor="white" strokeColor="white" cursor="ui-cursor-pointer" />}
           url="news/bulletins"
         />
         <SectionCard
-          className="h-[120px] w-[130px] rounded-3xl bg-pink-300 shadow-lg duration-700 hover:bg-pink-200"
+          className="h-[120px] w-[130px] rounded-3xl bg-blue-400 shadow-lg duration-700"
           text="예배 안내"
           icon={<Icon name="Bell" size="xl" backgroundColor="white" strokeColor="white" cursor="ui-cursor-pointer" />}
           url="about/services"
         />
         <SectionCard
-          className="h-[120px] w-[130px] rounded-3xl bg-pink-300 shadow-lg duration-700 hover:bg-pink-200"
+          className="h-[120px] w-[130px] rounded-3xl bg-blue-400 shadow-lg duration-700"
           text="새가족 안내"
           icon={
             <Icon name="HandHeart" size="xl" backgroundColor="white" strokeColor="white" cursor="ui-cursor-pointer" />
