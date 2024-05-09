45cc0127af30ec34faa78d608d40fc81d827986c
ChoiHyuk <wannabeing@naver.com>
2024-05-09 15:24:37 +0900

    chore: 메인 Nav 섹션 스타일 수정

--- apps/client/src/components/home/section/nav.tsx
+++ apps/client/src/components/home/section/nav.tsx
@@ -15,7 +15,7 @@ const NavSection = () => {
   }[] = [
     {
       titleKr: "교회 소개",
-      titleEn: "Worship information",
+      titleEn: "About Myungmoon",
       description: "지역과 민족을 품고\n세계와 열방을 향해 나아가는\n명문교회를 소개합니다.",
       path: "/about",
       img: "/images/sermon/banner2.jpg",
@@ -23,7 +23,7 @@ const NavSection = () => {
     },
     {
       titleKr: "예배 안내",
-      titleEn: "Worship information",
+      titleEn: "Worship Information",
       description: "평일예배와 주일예배의\n시간과 장소를\n안내해드립니다.",
       path: "/about/services",
       img: "/images/sermon/banner2.jpg",
@@ -31,7 +31,7 @@ const NavSection = () => {
     },
     {
       titleKr: "주보",
-      titleEn: "Worship information",
+      titleEn: "Bulletin",
       description: "매주/매월 진행되는\n사역과 소식을\n안내해드립니다.",
       path: "/news/bulletins",
       img: "/images/home/focus.jpg",
@@ -39,7 +39,7 @@ const NavSection = () => {
     },
     {
       titleKr: "다음 세대",
-      titleEn: "Worship information",
+      titleEn: "Next Generation",
       description: "하나님을 영화롭게 하며\n하나님을 깊이 만나는\n다음세대를 소개합니다.",
       path: "/education",
       img: "/images/education/banner.jpg",
@@ -54,14 +54,13 @@ const NavSection = () => {
         <Image src="/images/home/section3.jpg" alt="" fill={true} className="object-cover" />
       </div>
       <div className="absolute left-0 top-0 z-[1] flex w-full flex-col gap-12 px-8 py-16 md:gap-32">
-        <div className="flex flex-col gap-8 text-white">
+        <div className="flex flex-col gap-3 sm:gap-8 text-white">
           <div data-aos="fade-up" className="h-[5px] w-[120px] bg-white" />
-          <p data-aos="fade-up" className="font-SCoreDream w-3/4 text-xl font-bold md:text-5xl">
-            명문교회 홈페이지를 방문해주신 여러분을
-            <br />
-            주님의 이름으로 환영하고 축복합니다.
-          </p>
-          <p data-aos="fade-up" className="font-Yanone text-xl leading-5 md:text-4xl">
+          <div data-aos="fade-up" className="flex flex-col sm:gap-1 font-SCoreDream w-full sm:text-xl font-bold md:text-3xl lg:text-4xl">
+            <p>명문교회 홈페이지를 방문해주신 여러분을</p>
+            <p>주님의 이름으로 환영하고 축복합니다.</p>
+          </div>
+          <p data-aos="fade-up" className="font-Cormorant text-xl leading-5 md:text-4xl">
             Thank you for visiting
             <br />
             the Myungmoon Church website.
@@ -71,7 +70,7 @@ const NavSection = () => {
             in the name of the Lord.
           </p>
         </div>
-        <div className="grid w-full grid-cols-2 gap-3 md:grid-cols-4">
+        <div className="grid w-full grid-cols-2 gap-10 md:grid-cols-4 md:gap-3">
           {navList.map((nav, idx) => (
             <div
               onClick={() => push(nav.path)}
@@ -79,39 +78,20 @@ const NavSection = () => {
               data-aos-offset="20"
               data-aos-delay={idx * 50}
               data-aos-duration="400"
-              className="flex h-full w-full cursor-pointer items-center justify-center text-white sm:gap-16"
+              className="flex h-full w-full cursor-pointer items-center justify-center text-white sm:gap-16 hover:text-blue-600"
             >
               <div className="flex flex-col items-center justify-center gap-5">
                 <div className="flex flex-col items-center justify-center gap-1">
-                  <p className="font-SCoreDream text-white transition-colors hover:text-blue-600 hover:underline sm:text-5xl md:text-4xl lg:text-6xl">
+                  <p className="font-SCoreDream transition-colors sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
                     {nav.titleKr}
                   </p>
-                  <p className="font-Lora hidden text-white md:text-lg lg:block">{nav.titleEn}</p>
+                  <p className="font-Lora hidden lg:text-lg md:block">{nav.titleEn}</p>
                 </div>
-                <p className="whitespace-pre text-center text-xs text-gray-300 md:text-base">{nav.description}</p>
+                <p className="whitespace-pre text-center text-xs md:text-base">{nav.description}</p>
               </div>
             </div>
           ))}
         </div>
-        {/* <div className="flex w-full items-center gap-8">
-          <div data-aos="fade-in" className="relative hidden h-[300px] w-1/2 md:block md:h-[300px] lg:h-[300px]">
-            <Image src="/images/home/focus.jpg" alt="" fill={true} className="object-cover" />
-          </div>
-          <div className="flex w-full flex-col gap-5 md:w-1/2">
-            {navList.map((nav, idx) => (
-              <p
-                data-aos="fade-up"
-                data-aos-offset="20"
-                data-aos-delay={idx * 50}
-                data-aos-duration="400"
-                className="font-SCoreDream w-fit cursor-pointer text-5xl font-bold text-[#a0a0a0] transition-colors hover:text-white hover:underline"
-                key={nav.label}
-              >
-                {nav.label}
-              </p>
-            ))}
-          </div>
-        </div> */}
       </div>
     </div>
   );
