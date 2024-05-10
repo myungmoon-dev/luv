308870ea56f1d994b177211a4d1f1286b40f5e13
ChoiHyuk <wannabeing@naver.com>
2024-05-10 16:59:56 +0900

    chore: 설교찬양탭 간단 디자인 수정

--- apps/client/src/components/sermons/sermon.tsx
+++ apps/client/src/components/sermons/sermon.tsx
@@ -8,9 +8,15 @@ interface ISermonProps {
 const Sermon = ({ sermon }: ISermonProps) => {
   return (
     <div className="flex w-full flex-col">
-      <YoutubeVideo className="mb-3 flex h-[300px] items-center justify-center lg:h-[200px]" videoId={sermon.videoId} />
-      <h1 className="font-semibold md:text-xl">{sermon.title}</h1>
-      <p className="text-sm text-gray-700">{`${sermon.preacher} | ${sermon.mainText}`}</p>
+      <YoutubeVideo
+        className="mb-3 flex h-[200px] items-center justify-center md:h-[400px] lg:h-[300px] xl:h-[400px] 2xl:h-[300px]"
+        videoId={sermon.videoId}
+      />
+      <h1 className="font-SCoreDream md:text-lg lg:text-xl">{sermon.title}</h1>
+      <div className="flex gap-1 text-sm text-gray-700 lg:text-lg">
+        <p>{sermon.preacher}</p>
+        {sermon.mainText && <p>{` | ${sermon.mainText}`}</p>}
+      </div>
     </div>
   );
 };
