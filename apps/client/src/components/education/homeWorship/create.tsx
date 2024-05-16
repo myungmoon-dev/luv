f0ac08335054c4c764aa1b3e513787115ba11860
NiNinanana <yeen00123@gmail.com>
2024-05-16 16:47:01 +0900

    feat: 추가 후 spinner 추가

--- apps/client/src/components/education/homeWorship/create.tsx
+++ apps/client/src/components/education/homeWorship/create.tsx
@@ -4,13 +4,14 @@ import { useRouter } from "next/navigation";
 import React from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { IHomeWorshipForm } from "type";
+import { Spinner, cn } from "ui";
 
 const HomeWorshipCreate = () => {
   const { push } = useRouter();
   const { register, handleSubmit } = useForm<IHomeWorshipForm>();
   const uid = useAuthStore((state) => state.uid);
 
-  const { mutate } = usePostHomeWorship();
+  const { mutate, isPending } = usePostHomeWorship();
 
   const onSubmit: SubmitHandler<IHomeWorshipForm> = async (data) => {
     const formData = new FormData();
@@ -32,16 +33,20 @@ const HomeWorshipCreate = () => {
       formData.append("userId", uid);
 
       mutate(formData, {
-        onSuccess: (res) => console.log(res),
-        onError: (err) => console.log(err),
-        onSettled: () => alert("완료"),
+        onSuccess: (res) => {
+          alert("추가되었습니다.");
+          push("/education/home-worship");
+        },
+        onError: (err) => {
+          alert("에러가 발생했습니다. 다시 시도해주세요.");
+        },
       });
     }
   };
 
   return (
-    <div className="flex justify-center">
-      <div className="flex w-[500px] flex-col gap-5 rounded-md px-5 py-8 shadow-lg">
+    <div className="relative flex justify-center">
+      <div className={cn("flex w-[500px] flex-col gap-5 rounded-md px-5 py-8 shadow-lg", isPending && "opacity-50")}>
         <h1 className="text-2xl">가정예배 인증하기</h1>
         <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
           <label className="flex items-center gap-5">
@@ -54,6 +59,9 @@ const HomeWorshipCreate = () => {
           </label>
           <button className="rounded-md bg-blue-500 py-2 text-white">제출</button>
         </form>
+      </div>
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <Spinner loading={isPending} />
       </div>
     </div>
   );
