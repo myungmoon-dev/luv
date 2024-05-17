3d17b13fc11e3ec54cf3eb8d8f414eb0b98a15a9
NiNinanana <yeen00123@gmail.com>
2024-05-17 17:50:54 +0900

    feat: homeworship 삭제하는 기능 연결

--- apps/client/src/components/education/homeWorship/detail.tsx
+++ apps/client/src/components/education/homeWorship/detail.tsx
@@ -1,12 +1,17 @@
-import { useGetHomeWorship } from "@/query/homeWorship";
+import { useDeleteHomeWorship, useGetHomeWorship } from "@/query/homeWorship";
+import useAuthStore from "@/store/auth";
 import dayjs from "dayjs";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { SafeHTML, Spinner } from "ui";
 
 const HomeWorshipDetail = () => {
+  const { push } = useRouter();
   const params = useParams();
   const homeWorshipId = params?.id as string;
+  const userId = useAuthStore((state) => state.uid);
+
   const { data } = useGetHomeWorship({ homeWorshipId });
+  const { mutate } = useDeleteHomeWorship();
 
   if (!data)
     return (
@@ -17,13 +22,38 @@ const HomeWorshipDetail = () => {
 
   const homeWorship = data.homeWorship;
 
+  const isUsersHomeWorship = userId === homeWorship.userId;
+
+  const handleClickDelete = () => {
+    if (!confirm("삭제하시겠습니까?")) return;
+    mutate(
+      { homeWorshipId },
+      {
+        onSuccess: () => {
+          alert("삭제되었습니다.");
+          push("/education/home-worship");
+        },
+        onError: (err: any) => {
+          alert(err.response.data.result);
+        },
+      },
+    );
+  };
+
   return (
     <div className="px-5 sm:px-10 md:px-20 lg:px-28 xl:px-36 2xl:px-40">
       <h1 className="mb-2 text-3xl font-bold">{homeWorship.title}</h1>
-      <div className="flex gap-2">
-        <p className="mb-10 text-sm text-slate-500">예배일: {dayjs(homeWorship.date).format("YYYY-MM-DD")}</p>
-        <p className="mb-10 text-sm text-slate-500">|</p>
-        <p className="mb-10 text-sm text-slate-500">생성일: {dayjs(homeWorship.createdAt).format("YYYY-MM-DD")}</p>
+      <div className="flex justify-between">
+        <div className="flex gap-2">
+          <p className="mb-10 text-sm text-slate-500">예배일: {dayjs(homeWorship.date).format("YYYY-MM-DD")}</p>
+          <p className="mb-10 text-sm text-slate-500">|</p>
+          <p className="mb-10 text-sm text-slate-500">생성일: {dayjs(homeWorship.createdAt).format("YYYY-MM-DD")}</p>
+        </div>
+        {isUsersHomeWorship && (
+          <button onClick={handleClickDelete} className="mb-10 text-sm text-red-500">
+            삭제
+          </button>
+        )}
       </div>
       <div className="mb-10">
         <SafeHTML html={homeWorship.content} />
