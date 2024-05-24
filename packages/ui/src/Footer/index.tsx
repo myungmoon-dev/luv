068caf28de0b08eebe896934b330f117ca916f41
ChoiHyuk <wannabeing@naver.com>
2024-05-24 13:14:13 +0900

    Chore/footer (#111)
    * chore: instagram/youtube 아이콘 stroke 설정
    
    * chore: footer 스타일 및 링크 설정
    
    * chore: a태그 target 속성 추가

--- packages/ui/src/Footer/index.tsx
+++ packages/ui/src/Footer/index.tsx
@@ -22,25 +22,51 @@ export const Footer = ({ push }: IFooterProps) => {
       <div className="ui-flex ui-items-center ui-gap-5">
         <Icon name="LogoBlue" size="4xl" />
         <div className="ui-flex ui-gap-2">
-          <Icon name="RealYoutube" size="xl" strokeColor="white" backgroundColor="white" />
-          <Icon name="Instagram" size="xl" strokeColor="white" backgroundColor="white" />
+          <a
+            href="https://www.youtube.com/channel/UC1v4PcaTti7luD1pYj_AgDg"
+            target="_blank"
+          >
+            <Icon
+              name="RealYoutube"
+              size="xl"
+              strokeColor="white"
+              backgroundColor="white"
+            />
+          </a>
+          <a
+            href="https://www.instagram.com/myungmoonchurch?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+            target="_blank"
+          >
+            <Icon
+              name="Instagram"
+              size="xl"
+              strokeColor="white"
+              backgroundColor="white"
+            />
+          </a>
         </div>
       </div>
-      <nav className="ui-flex ui-gap-5">
+      <nav className="ui-flex ui-gap-5 ui-text-sm sm:ui-text-base">
         {menus.map((menu) => (
-          <button onClick={() => push(menu.path)} key={menu.path} className="ui-font-semibold ui-text-md">
+          <button
+            onClick={() => push(menu.path)}
+            key={menu.path}
+            className="ui-font-semibold ui-text-md"
+          >
             {menu.label}
           </button>
         ))}
       </nav>
-      <div className="ui-flex ui-flex-col ui-text-sm ui-items-center">
-        <div className="flex gap-3">
+      <div className="ui-flex ui-flex-col ui-items-center ui-gap-1">
+        <div className="ui-flex ui-gap-3 ui-text-xs sm:ui-text-base">
           <p>TEL. 02-861-5071</p>
           <p>서울특별시 금천구 남부순환로 1406 명문교회</p>
         </div>
-        <p>Copyright © 2024 명문교회 All rights reserved.</p>
+        <p className="ui-text-sm sm:ui-text-base">
+          Copyright © 2024 명문교회 All rights reserved.
+        </p>
       </div>
-      <div className="ui-flex ui-gap-x-2 ui-flex-wrap ui-text-white">
+      <div className="ui-flex ui-gap-x-2 ui-flex-wrap ui-text-white ui-text-sm sm:ui-text-base">
         <button onClick={handleNotPreparedClick} className="hover:ui-underline">
           이용약관
         </button>
