6787ecedf9eb28198773c84a3d9383b0daa0804e
NiNinanana <yeen00123@gmail.com>
2024-05-27 10:01:51 +0900

    refactor: 어드민 성경통독 목록에도 날짜 탭 추가

--- apps/admin/src/pages/api/discipleship/bibles/index.ts
+++ apps/admin/src/pages/api/discipleship/bibles/index.ts
@@ -1,15 +1,19 @@
 import { getBibles, postBible } from "firebase";
 import { NextApiRequest, NextApiResponse } from "next";
+import { YearMonthType } from "type";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const {
     method,
     body: { content, date, title, links },
+    query,
   } = req;
+
+  const yearMonth = query.yearMonth as YearMonthType;
 
   switch (method) {
     case "GET":
-      const bibles = (await getBibles()).docs.map((doc) => ({ ...doc.data(), id: doc.id })) || [];
+      const bibles = (await getBibles({ yearMonth })).docs.map((doc) => ({ ...doc.data(), id: doc.id })) || [];
       return res.status(200).json({
         bibles,
       });
