a0ac36c0e81c9414633cba822ce4d477ee5633e0
ChoiHyuk <wannabeing@naver.com>
2024-05-28 17:08:46 +0900

    chore: 메인 표어 굵기 조정

--- apps/client/src/components/home/banner/watchword.tsx
+++ apps/client/src/components/home/banner/watchword.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HomeBannerWatchword = () => {
   return (
-    <h1 className="text-center text-4xl font-extrabold text-white sm:text-[3.75rem] sm:leading-[4.5rem]">
+    <h1 className="text-center text-4xl text-white sm:text-[3.75rem] sm:leading-[4.5rem]">
       <span data-aos="fade-up" className="font-SCoreDream">교회여 일어나!</span>
       <br />
       <span data-aos="fade-up" className="font-SCoreDream">세상으로 흘러가라!</span>
