6905ca6fbd3200469a21e34ed9822c74552b7dd1
ChoiHyuk <wannabeing@naver.com>
2024-05-28 17:51:10 +0900

    chore: 교회앨범/당회소식 삭제 (추후 추가예정)

--- apps/client/src/constants/innerMenus/news.ts
+++ apps/client/src/constants/innerMenus/news.ts
@@ -2,8 +2,6 @@ import { IInnerMenu } from "./types";
 
 export const newsInnerMenus: IInnerMenu[] = [
   { label: "주보", path: "/news/bulletins" },
-  { label: "교회 앨범", path: "/news/photos" },
-  { label: "당회소식", path: "/news/session-news" },
   { label: "선교지소식", path: "/news/mission-news" },
   { label: "자료함", path: "/news/resources" },
 ];
