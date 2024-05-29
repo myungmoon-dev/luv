8e61af396db34ff660898db9288ca236e687cd3d
NiNinanana <yeen00123@gmail.com>
2024-05-29 16:27:08 +0900

    feat: packages/helper 추가 및 useDateTab 커스텀 훅 생성

---
+++ packages/helper/src/useDateTab/index.ts
@@ -0,0 +1,58 @@
+import { YearMonthType } from "type";
+import { produce } from "immer";
+import { Dispatch, SetStateAction } from "react";
+
+const getPreviousMonth = (date: YearMonthType): YearMonthType => {
+  const [year, month] = date.split("-").map(Number) as [number, number];
+  const newDate = new Date(year, month - 1, 1);
+  newDate.setMonth(newDate.getMonth() - 1);
+  const newYear = newDate.getFullYear();
+  const newMonth = (newDate.getMonth() + 1).toString().padStart(2, "0");
+  return `${newYear}-${newMonth}`;
+};
+
+const getNextMonth = (date: YearMonthType): YearMonthType => {
+  const [year, month] = date.split("-").map(Number) as [number, number];
+  const newDate = new Date(year, month - 1, 1);
+  newDate.setMonth(newDate.getMonth() + 1);
+  const newYear = newDate.getFullYear();
+  const newMonth = (newDate.getMonth() + 1).toString().padStart(2, "0");
+  return `${newYear}-${newMonth}`;
+};
+
+interface IUseDateTabProps {
+  setDateTabs: Dispatch<
+    SetStateAction<
+      {
+        date: YearMonthType;
+        label: string;
+      }[]
+    >
+  >;
+}
+
+export const useDateTab = ({ setDateTabs }: IUseDateTabProps) => {
+  const handleClickPrev = () => {
+    setDateTabs(
+      produce((draft) => {
+        draft.pop();
+        const newDate = getPreviousMonth(draft[0]!.date);
+        const newLabel = `${newDate.split("-")[1]}월`;
+        draft.unshift({ label: newLabel, date: newDate });
+      })
+    );
+  };
+
+  const handleClickNext = () => {
+    setDateTabs(
+      produce((draft) => {
+        draft.shift();
+        const newDate = getNextMonth(draft[draft.length - 1]!.date);
+        const newLabel = `${newDate.split("-")[1]}월`;
+        draft.push({ label: newLabel, date: newDate });
+      })
+    );
+  };
+
+  return { handleClickPrev, handleClickNext };
+};
