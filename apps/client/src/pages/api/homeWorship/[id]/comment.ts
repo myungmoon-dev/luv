9f40d18f1e0790defe943f9a21fbadde6e578be5
NiNinanana <yeen00123@gmail.com>
2024-06-01 19:40:38 +0900

    feat: 댓글 올리고 지우는 api 추가

---
+++ apps/client/src/pages/api/homeWorship/[id]/comment.ts
@@ -0,0 +1,54 @@
+import { deleteHomeWorshipComment, getHomeWorship, postHomeWorshipComment } from "firebase";
+import { NextApiRequest, NextApiResponse } from "next";
+import { comparePassword, hashPassword } from "helper";
+import { v4 as uuidv4 } from "uuid";
+import { IComment } from "type";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method, query, body } = req;
+
+  const name = body?.name;
+  const password = body?.password;
+  const commentId = body?.commentId;
+  const content = body?.content;
+
+  const homeWorshipId = query.id as string;
+
+  switch (method) {
+    case "POST": {
+      const hashedPassword = await hashPassword(password);
+
+      const result = await postHomeWorshipComment({
+        homeWorshipId,
+        comment: { id: uuidv4(), name, password: hashedPassword, content, createdAt: new Date().getTime() },
+      });
+
+      return res.status(200).json({
+        result,
+      });
+    }
+
+    case "DELETE": {
+      const homeWorship = (await getHomeWorship(homeWorshipId)).data();
+      const comment = homeWorship?.comments.filter((comment: IComment) => comment.id === commentId)[0] as IComment;
+      const commentPassword = comment.password;
+
+      const isValidPassword = await comparePassword(password, commentPassword);
+
+      if (!isValidPassword)
+        return res.status(403).json({
+          result: "비밀번호가 일치하지 않습니다.",
+        });
+
+      const snapshot = await deleteHomeWorshipComment({ comment, homeWorshipId });
+
+      return res.status(200).json({
+        result: snapshot,
+      });
+    }
+
+    default:
+      res.setHeader("Allow", ["POST"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
