a4bfceb8dcc95de21105b194ad473a702970b23e
NiNinanana <yeen00123@gmail.com>
2024-06-01 19:38:56 +0900

    feat: comment 타입 추가

--- packages/type/src/index.ts
+++ packages/type/src/index.ts
@@ -1,16 +1,12 @@
 import { IBulletin, IBulletinForm, IBulletinImageForm } from "./bulletin";
-import {
-  YoutubeType,
-  IYoutube,
-  IYoutubeForm,
-  IGetYoutubeResponse,
-} from "./youtube";
+import { YoutubeType, IYoutube, IYoutubeForm, IGetYoutubeResponse } from "./youtube";
 import { IBible, IBibleForm } from "./bible";
 import { IHomeWorship, IHomeWorshipForm } from "./homeWorship";
 import { YearMonthType } from "./date";
 import { IMission, IMissionForm } from "./news";
 import { IUser } from "./user";
 import { IDiscipleshipTabData } from "./discipleship";
+import { IComment } from "./comment";
 
 export type {
   IBulletin,
@@ -29,4 +25,5 @@ export type {
   IMissionForm,
   IUser,
   IDiscipleshipTabData,
+  IComment,
 };
