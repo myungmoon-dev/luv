d0e4cf138762c66056c82041604cb81289278b42
ChoiHyuk <wannabeing@naver.com>
2024-06-03 17:39:59 +0900

    feat: 원로목사 탭 생성

--- apps/client/src/pages/about/leadership/retired.tsx
+++ apps/client/src/pages/about/leadership/retired.tsx
@@ -1,7 +1,8 @@
 import Layout from "@/components/layout";
 import { aboutInnerMenus, aboutLeaderMenus } from "@/constants/innerMenus/about";
 import Tabs from "@/components/layout/tabs";
-import Image from "next/image";
+import ProfileList from "@/components/about/profileList";
+import CustomImage from "@/components/customImage";
 
 const LeadershipRetiredPage = () => {
   return (
@@ -15,16 +16,22 @@ const LeadershipRetiredPage = () => {
       detailMenus={aboutLeaderMenus}
     >
       <Tabs menus={aboutLeaderMenus}>
-        <div className="flex items-start justify-center gap-10 px-3 pb-5">
-          <div className="relative flex h-[250px] w-[200px] items-center justify-center overflow-hidden rounded-xl shadow-2xl ">
-            <Image src="/images/leader/deok-jin.jpeg" alt="이덕진 원로목사" fill className="object-cover" />
+        <div className="relative flex w-full items-center justify-center px-3 md:gap-5">
+          <div className="z-[1] flex flex-col justify-center gap-3">
+            <h1 className="font-SCoreDream text-2xl text-blue-600 xl:text-5xl">이덕진 원로목사</h1>
+            <p className="font-SCoreDream xl:text-2xl">명문교회 원로목사</p>
+            <p className="whitespace-pre-wrap break-keep text-xs md:text-sm lg:text-base xl:text-lg">
+              대한예수교장로회 (합동) GMS 명예선교사, 꿈을꾸는세계선교회 대표
+            </p>
           </div>
-          <div className="flex max-w-sm flex-grow flex-col gap-5 pt-10">
-            <div className="flex flex-col items-center gap-3 border-b-[1px] border-gray-300 pb-2 md:flex-row">
-              <h2 className="text-lg font-bold md:text-2xl">이덕진</h2>
-              <p className="text-sm font-[500] md:text-xl">원로목사</p>
-            </div>
-          </div>
+          <CustomImage
+            src={"/images/leader/deok-jin.png"}
+            alt="이덕진 원로목사"
+            className="z-[1] h-[200px] w-[150px] xl:h-[400px] xl:w-[300px]"
+            imgClass="object-[50%_0%]"
+            quality={30}
+          />
+          <div className="absolute h-full w-[190px] rounded-full bg-gray-200 md:w-[200px] xl:w-[400px]" />
         </div>
       </Tabs>
     </Layout>
