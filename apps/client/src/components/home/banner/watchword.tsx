d9f64b20b01aa9db2e34e03bc1000b928be8b37c
ChoiHyuk <wannabeing@naver.com>
2024-06-04 09:04:29 +0900

    Chore/main (#133)
    * chore: 메인 설교섹션 말씀제목 가운데정렬
    
    * refactor: 카카오맵 컴포넌트 높이 설정가능하도록 수정
    
    * chore: 느낌표 위치 수정

--- apps/client/src/components/home/banner/watchword.tsx
+++ apps/client/src/components/home/banner/watchword.tsx
@@ -3,9 +3,13 @@ import React from "react";
 const HomeBannerWatchword = () => {
   return (
     <h1 className="text-center text-4xl text-white sm:text-[3.75rem] sm:leading-[4.5rem]">
-      <span data-aos="fade-up" className="font-SCoreDream">교회여 일어나!</span>
+      <span data-aos="fade-up" className="font-SCoreDream">
+        교회여! 일어나
+      </span>
       <br />
-      <span data-aos="fade-up" className="font-SCoreDream">세상으로 흘러가라!</span>
+      <span data-aos="fade-up" className="font-SCoreDream">
+        세상으로 흘러가라!
+      </span>
     </h1>
   );
 };
