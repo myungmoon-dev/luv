467a68476685908368c869c80bea71109e215ef6
ChoiHyuk <wannabeing@naver.com>
2024-06-04 18:00:03 +0900

    chore: 띄어쓰기 텍스트 수정

--- apps/client/src/constants/innerMenus/discipleship.ts
+++ apps/client/src/constants/innerMenus/discipleship.ts
@@ -1,7 +1,7 @@
 import { IInnerMenu } from "./types";
 
 export const discipleshipInnerMenus: IInnerMenu[] = [
-  { label: "교회 훈련", path: "/discipleship/main/bible" },
+  { label: "교회훈련", path: "/discipleship/main/bible" },
   { label: "새가족 교육", path: "/discipleship/new" },
   { label: "신혼가정", path: "/discipleship/newlyweds" },
   { label: "3040세대", path: "/discipleship/3040" },
