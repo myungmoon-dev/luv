5ac50433f2c9249b0e438128a007590557949074
ChoiHyuk <wannabeing@naver.com>
2024-06-07 16:29:39 +0900

    feat: 3040세대 관련 타입 추가

--- packages/type/src/discipleship.ts
+++ packages/type/src/discipleship.ts
@@ -4,3 +4,47 @@ export interface INewCommersData {
   text?: string;
   caution?: string;
 }
+
+export type GENERATION_3040_TYPE =
+  | "watchword"
+  | "vision"
+  | "purpose"
+  | "program";
+
+export interface I3040Data {
+  id: GENERATION_3040_TYPE;
+  data: any; // 데이터 타입을 정확하게 정의하기 위해 아래와 같은 세부 인터페이스를 사용
+}
+export interface I3040WatchwordData extends I3040Data {
+  data: {
+    text1: string;
+    text2: string;
+    text3: string;
+  };
+}
+export interface I3040VisionData extends I3040Data {
+  data: {
+    img: string;
+    text1: string;
+    text2?: string;
+    emphasis: string;
+  };
+}
+
+export interface I3040PurposeData extends I3040Data {
+  data: {
+    img: string;
+    purposes: string[];
+  };
+}
+
+export interface I3040ProgramData extends I3040Data {
+  data: {
+    img: string;
+    programs: I3040ProgramInfo[];
+  };
+}
+export interface I3040ProgramInfo {
+  title: string;
+  informations: string[];
+}
--- packages/type/src/index.ts
+++ packages/type/src/index.ts
@@ -10,7 +10,16 @@ import { IHomeWorship, IHomeWorshipForm } from "./homeWorship";
 import { YearMonthType } from "./date";
 import { IMission, IMissionForm } from "./news";
 import { IUser } from "./user";
-import { INewCommersData } from "./discipleship";
+import {
+  INewCommersData,
+  GENERATION_3040_TYPE,
+  I3040Data,
+  I3040ProgramData,
+  I3040ProgramInfo,
+  I3040PurposeData,
+  I3040VisionData,
+  I3040WatchwordData,
+} from "./discipleship";
 
 export type {
   IBulletin,
@@ -29,4 +38,11 @@ export type {
   IMissionForm,
   IUser,
   INewCommersData,
+  GENERATION_3040_TYPE,
+  I3040Data,
+  I3040ProgramData,
+  I3040ProgramInfo,
+  I3040PurposeData,
+  I3040VisionData,
+  I3040WatchwordData,
 };
