0eb920f467f2f2ae440562cc8d2b9ac7f89122cb
ChoiHyuk <wannabeing@naver.com>
2024-06-07 13:59:42 +0900

    refactor: 기존 파노라마 관련 컴포넌트 삭제 및 공통 컴포넌트 tabs로 변경

--- apps/client/src/pages/discipleship/main/panorama.tsx
+++ apps/client/src/pages/discipleship/main/panorama.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from "@/components/layout";
 import { discipleshipInnerMenus, discipleshipMainMenus } from "@/constants/innerMenus/discipleship";
 import Tabs from "@/components/layout/tabs";
-import DiscipleshipPanorama from "@/components/discipleship/main/panorama/section";
+import DiscipleshipPanorama from "@/components/discipleship/main/panorama";
 
 const DiscipleshipMainPanoramaPage = () => {
   return (
