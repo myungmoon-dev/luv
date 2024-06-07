701d5779d0cee56cde0a3d69f21d7256084e4313
ChoiHyuk <wannabeing@naver.com>
2024-06-07 18:00:48 +0900

    feat: 신혼가정 탭 관련 컴포넌트 생성 및 적용

---
+++ apps/client/src/components/discipleship/newlyweds/section/introduce.tsx
@@ -0,0 +1,12 @@
+interface INewlywedsIntroduceSectionProps {
+  text: string;
+}
+const NewlywedsIntroduceSection = ({ text }: INewlywedsIntroduceSectionProps) => {
+  return (
+    <div data-aos="fade-up" className="flex flex-col items-center justify-center gap-1 md:w-1/2 md:gap-3 xl:gap-10">
+      <h1 className="font-SCoreDream text-2xl text-blue-600 md:text-3xl xl:text-5xl">신혼가정 | Newlyweds</h1>
+      <p className="break-keep text-center md:text-lg xl:text-2xl">{text}</p>
+    </div>
+  );
+};
+export default NewlywedsIntroduceSection;
