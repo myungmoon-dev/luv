052030749be33387da20a0f15b2d76f692751870
macmini <wannabeing@naver.com>
2024-06-08 21:00:06 +0900

    chore: 중복 수정

--- packages/type/src/index.ts
+++ packages/type/src/index.ts
@@ -1,12 +1,16 @@
 import { IBulletin, IBulletinForm, IBulletinImageForm } from "./bulletin";
-import { YoutubeType, IYoutube, IYoutubeForm, IGetYoutubeResponse } from "./youtube";
+import {
+  YoutubeType,
+  IYoutube,
+  IYoutubeForm,
+  IGetYoutubeResponse,
+} from "./youtube";
 import { IBible, IBibleForm } from "./bible";
 import { IHomeWorship, IHomeWorshipForm } from "./homeWorship";
 import { YearMonthType } from "./date";
 import { IMission, IMissionForm } from "./news";
 import { IUser } from "./user";
 import {
-  IDiscipleshipTabData,
   IDiscipleshipTabData,
   INewlywedsCurriculumData,
   INewlywedsData,
