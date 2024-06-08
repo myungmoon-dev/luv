fc1df466001f625b3db30529c12dc83a3db99183
macmini <wannabeing@naver.com>
2024-06-08 20:27:13 +0900

    refactor: 신혼가정 타입 제네릭으로 수정

--- packages/type/src/discipleship.ts
+++ packages/type/src/discipleship.ts
@@ -7,35 +7,26 @@ export interface IDiscipleshipTabData {
 
 export type NewlywedsType = "introduce" | "images" | "guide" | "curriculum";
 
-export interface INewlywedsData {
+export interface INewlywedsData<T> {
   id: NewlywedsType;
-  data: any;
+  data: T;
 }
 
-export interface INewlywedsIntroData extends INewlywedsData {
-  data: {
-    text: string;
-  };
-}
+export interface INewlywedsIntroData extends INewlywedsData<{ text: string }> {}
 
-export interface INewlywedsImageData extends INewlywedsData {
-  data: {
-    imgs: string[];
-  };
-}
+export interface INewlywedsImageData
+  extends INewlywedsData<{ imgs: string[] }> {}
 
-export interface INewlywedsGuideData extends INewlywedsData {
-  data: {
+export interface INewlywedsGuideData
+  extends INewlywedsData<{
     target: { title: string; description: string };
     time: { title: string; description: string };
     place: { title: string; description: string };
     manager: { name: string; number: string };
-  };
-}
+  }> {}
 
-export interface INewlywedsCurriculumData extends INewlywedsData {
-  data: {
+export interface INewlywedsCurriculumData
+  extends INewlywedsData<{
     firstHalf: { title: string; content: string[] };
     secondHalf: { title: string; content: string[] };
-  };
-}
+  }> {}
