421cb684a3f8800b2976d0fa2d04149002c490ba
macmini <wannabeing@naver.com>
2024-06-08 20:21:28 +0900

    chore: 코드 정리

--- apps/client/src/components/about/coreLabel.tsx
+++ apps/client/src/components/about/coreLabel.tsx
@@ -10,9 +10,11 @@ const AboutCoreLabel = ({ type }: IAboutCoreLabelProps) => {
       data-aos="fade-right"
       className="flex w-full flex-col items-center justify-center gap-5 bg-blue-600 py-12 text-white md:py-20"
     >
-      <p className="text-xl font-SCoreDream sm:text-4xl md:text-4xl lg:text-5xl md:font-medium">명문교회 2024 핵심가치와 비전</p>
-      <div className="flex w-full justify-center items-end gap-3 md:gap-7">
-        <p className="font-bold text-2xl md:text-5xl lg:text-7xl">{labelTitle}</p>
+      <p className="font-SCoreDream text-xl sm:text-4xl md:text-4xl md:font-medium lg:text-5xl">
+        명문교회 2024 핵심가치와 비전
+      </p>
+      <div className="flex w-full items-end justify-center gap-3 md:gap-7">
+        <p className="text-2xl font-bold md:text-5xl lg:text-7xl">{labelTitle}</p>
         <p className="text-xl md:text-5xl lg:text-7xl">|</p>
         <p className="font-Lora text-2xl font-bold md:text-5xl lg:text-7xl">{labelSubTitle}</p>
       </div>
