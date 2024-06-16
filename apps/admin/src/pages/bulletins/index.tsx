8d646cf31f618e23b4bc6c664e5eef7076670d25
NiNinanana <yeen00123@gmail.com>
2024-06-16 09:33:38 +0900

    feat: 어드민 주보 목록 페이지 추가

---
+++ apps/admin/src/pages/bulletins/index.tsx
@@ -0,0 +1,33 @@
+import { useGetBulletins } from "@/query/bulletin";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { Spinner, Table } from "ui";
+
+const BulletinsPage = () => {
+  const { push } = useRouter();
+
+  const { data } = useGetBulletins();
+
+  if (!data)
+    return (
+      <div className="flex justify-center">
+        <Spinner />
+      </div>
+    );
+
+  return (
+    <div className="px-24 py-10">
+      <Table
+        data={data?.bulletins.map((bulletin) => ({
+          id: bulletin.id,
+          date: bulletin.date,
+          title: bulletin.title,
+          writer: "관리자",
+        }))}
+        onClickRow={(rowId) => push(`/bulletins/${rowId}`)}
+      />
+    </div>
+  );
+};
+
+export default BulletinsPage;
