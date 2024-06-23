6cb4dcd93879f138b5f1f7ef36c2220c450664e9
강선우 <74632731+NiNinanana@users.noreply.github.com>
2024-06-23 09:47:41 +0900

    hotfix: main youtube error modal 나오도록 적용 (#160)

--- apps/client/src/components/home/index.tsx
+++ apps/client/src/components/home/index.tsx
@@ -1,9 +1,18 @@
+import useModalStore from "@/store/modal";
 import MapSection from "./section/map";
 import NavSection from "./section/nav";
 import OfferingSection from "./section/offering";
 import VideosSection from "./section/videos";
+import { useEffect } from "react";
+import YoutubeError from "./youtubeError";
 
 const HomePage = () => {
+  const openModal = useModalStore((state) => state.open);
+
+  useEffect(() => {
+    openModal(<YoutubeError />);
+  }, []);
+
   return (
     <div className="flex flex-col">
       <VideosSection />
---
+++ apps/client/src/components/home/youtubeError.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const YoutubeError = () => {
+  return (
+    <div>
+      <p>서울여상 인터센 선로문제로 인해 유튜브 송출이 안되고 있습니다. 불편을 드려 죄송합니다.</p>
+    </div>
+  );
+};
+
+export default YoutubeError;
