85a93078a7ec2268b50edbc2dc069e088ad6af36
강선우 <74632731+NiNinanana@users.noreply.github.com>
2024-06-23 10:14:46 +0900

    refactor: 유튜브 라이브 클릭 시 에러 모달 뜨도록 수정 (#162)

--- apps/client/src/components/home/banner/live.tsx
+++ apps/client/src/components/home/banner/live.tsx
@@ -1,27 +1,30 @@
 import { useGetYoutubeList } from "@/query/youtube";
+import useModalStore from "@/store/modal";
 import dayjs from "dayjs";
-import React from "react";
+import YoutubeError from "../youtubeError";
 
 const HomeBannerLive = () => {
   const { data } = useGetYoutubeList({ videoType: "live" });
   const liveVideo = data?.[0];
 
+  const openModal = useModalStore((state) => state.open);
+
+  const handleClickLive = () => {
+    openModal(<YoutubeError />);
+    return; // FIXME: 에러 풀릴 시 삭제
+    open(`https://youtu.be/${liveVideo?.videoId}`);
+  };
+
   return (
     <div className="flex flex-col items-center gap-6">
       <h1 className="text-center text-4xl font-extrabold text-white sm:text-[3.75rem] sm:leading-[4.5rem]">
-        <span data-aos="fade-up">
-          {dayjs(liveVideo?.date).format("YYYY.MM.DD")}
-        </span>
+        <span data-aos="fade-up">{dayjs(liveVideo?.date).format("YYYY.MM.DD")}</span>
         <br />
         <span data-aos="fade-up" className="font-SCoreDream">
           {liveVideo?.title} LIVE
         </span>
       </h1>
-      <button
-        onClick={() => open(`https://youtu.be/${liveVideo?.videoId}`)}
-        data-aos="fade-up"
-        className="text-2xl text-white hover:underline font-bold"
-      >
+      <button onClick={handleClickLive} data-aos="fade-up" className="text-2xl font-bold text-white hover:underline">
         예배생중계 바로가기 {">"}
       </button>
     </div>
