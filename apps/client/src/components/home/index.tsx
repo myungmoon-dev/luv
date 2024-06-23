4fce0e07c281c28864adac1e593f06e149ca430e
NiNinanana <yeen00123@gmail.com>
2024-06-23 10:14:22 +0900

    refactor: 유튜브 라이브 클릭 시 에러 모달 뜨도록 수정

--- apps/client/src/components/home/index.tsx
+++ apps/client/src/components/home/index.tsx
@@ -1,18 +1,9 @@
-import useModalStore from "@/store/modal";
 import MapSection from "./section/map";
 import NavSection from "./section/nav";
 import OfferingSection from "./section/offering";
 import VideosSection from "./section/videos";
-import { useEffect } from "react";
-import YoutubeError from "./youtubeError";
 
 const HomePage = () => {
-  const openModal = useModalStore((state) => state.open);
-
-  useEffect(() => {
-    openModal(<YoutubeError />);
-  }, []);
-
   return (
     <div className="flex flex-col">
       <VideosSection />
