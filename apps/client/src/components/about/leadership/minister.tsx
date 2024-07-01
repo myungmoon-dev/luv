62ef3ea6d2a104bbaf9f2ae7783a34afb148e0a2
ChoiHyuk <wannabeing@naver.com>
2024-07-01 09:35:45 +0900

    chore: 교역자탭 목사,전도사 구분 (#168)

--- apps/client/src/components/about/leadership/minister.tsx
+++ apps/client/src/components/about/leadership/minister.tsx
@@ -1,18 +1,22 @@
 import CustomImage from "@/components/customImage";
+import { officerType } from "@/constants/innerMenus/about";
 
 interface IMinisterProps {
   name: string;
   greeting: string;
   img: string;
   position: string;
+  officer: string;
 }
 
-const Minister = ({ name, greeting, img, position }: IMinisterProps) => {
+const Minister = ({ name, greeting, img, position, officer }: IMinisterProps) => {
   return (
     <div className="relative flex w-full items-center justify-center gap-3 md:gap-5">
       <div className="flex flex-col gap-3 md:w-1/3 2xl:w-1/4">
         <div className="flex items-center gap-1 border-b-[1px] border-gray-300 pb-[2px] 2xl:pb-2">
-          <p className="font-SCoreDream text-xl md:text-2xl 2xl:text-3xl">{name} 목사</p>
+          <p className="font-SCoreDream text-xl md:text-2xl 2xl:text-3xl">
+            {name} {officerType.filter((staff) => staff.label === officer)[0].type}
+          </p>
         </div>
         <p className="break-keep text-sm md:text-base">{greeting}</p>
         <div className="flex flex-col gap-1">
