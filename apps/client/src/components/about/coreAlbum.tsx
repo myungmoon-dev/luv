afe571ed44aa10858b913de9a6418d3b73da4e27
강선우 <74632731+NiNinanana@users.noreply.github.com>
2024-07-23 09:38:16 +0900

    feat: AboutCoreAlbum에 양쪽에 화살표를 두어 페이징 기능 추가 (#174)

--- apps/client/src/components/about/coreAlbum.tsx
+++ apps/client/src/components/about/coreAlbum.tsx
@@ -1,6 +1,6 @@
 import { AnimatePresence, motion } from "framer-motion";
+import { MouseEventHandler, useState } from "react";
 import CustomImage from "../customImage";
-import { useState } from "react";
 
 interface IAboutCoreAlbumProps {
   dataList: IValueVision[];
@@ -32,14 +32,26 @@ const coreValueVariants = {
 };
 
 const AboutCoreAlbum = ({ dataList }: IAboutCoreAlbumProps) => {
-  const [selectedValue, setSelectedValue] = useState<IValueVision>(dataList[0]);
+  const [selectedValue, setSelectedValue] = useState(0);
   const [showDetailValue, setDetailValue] = useState<boolean>(false);
+  const selectedData = dataList[selectedValue];
+  const isFirstValue = selectedValue === 0;
+  const isLastValue = selectedValue === dataList.length - 1;
 
   const toggleShowDetailValue = () => setDetailValue((prev) => !prev);
 
   const onClickDetailValue = (valueId: number) => {
-    setSelectedValue(dataList[valueId - 1]); // FIXME: id와 index는 -1차이
+    setSelectedValue(valueId - 1); // FIXME: id와 index는 -1차이
     toggleShowDetailValue();
+  };
+
+  const handleClickPrev: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.stopPropagation();
+    setSelectedValue((prev) => prev - 1);
+  };
+  const handleClickNext: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.stopPropagation();
+    setSelectedValue((prev) => prev + 1);
   };
 
   return (
@@ -77,7 +89,6 @@ const AboutCoreAlbum = ({ dataList }: IAboutCoreAlbumProps) => {
         {showDetailValue && (
           <AnimatePresence>
             <motion.div
-              // layoutId={selectedValue.id + ""}
               onClick={toggleShowDetailValue}
               className="z-99 absolute left-0 top-0 h-full w-full"
               variants={coreValueVariants}
@@ -87,24 +98,40 @@ const AboutCoreAlbum = ({ dataList }: IAboutCoreAlbumProps) => {
             >
               <CustomImage
                 className="relative h-[300px] w-full md:h-[500px]"
-                imgClass={selectedValue.imgClass}
-                src={selectedValue.img}
-                alt={`${selectedValue.titleKr} 이미지`}
+                imgClass={selectedData.imgClass}
+                src={selectedData.img}
+                alt={`${selectedData.titleKr} 이미지`}
               >
+                {!isFirstValue && (
+                  <button
+                    onClick={handleClickPrev}
+                    className="absolute left-3 top-1/2 z-10 -translate-y-1/2 text-3xl text-white md:left-5 xl:left-8"
+                  >
+                    {"<"}
+                  </button>
+                )}
                 <div className="absolute grid h-full w-full grid-cols-5 sm:gap-16">
                   <div className="col-span-2 flex items-center justify-center">
-                    <p className="font-SCoreDream text-9xl text-blue-600 md:text-[300px]">{selectedValue.id}</p>
+                    <p className="font-SCoreDream text-9xl text-blue-600 md:text-[300px]">{selectedData.id}</p>
                   </div>
                   <div data-aos="fade-left" className="col-span-3 flex w-full flex-col justify-center gap-6 md:gap-20">
                     <div className="flex flex-col gap-3 text-white">
-                      <p className="font-SCoreDream text-2xl sm:text-3xl md:text-5xl">{selectedValue.titleKr}</p>
-                      <p className="font-Lora sm:text-xl md:text-2xl lg:text-5xl">{selectedValue.titleEn}</p>
+                      <p className="font-SCoreDream text-2xl sm:text-3xl md:text-5xl">{selectedData.titleKr}</p>
+                      <p className="font-Lora sm:text-xl md:text-2xl lg:text-5xl">{selectedData.titleEn}</p>
                     </div>
                     <p className="break-keep pr-6 font-medium leading-relaxed text-white sm:pr-0 sm:text-2xl md:whitespace-pre-wrap">
-                      {selectedValue.description}
+                      {selectedData.description}
                     </p>
                   </div>
                 </div>
+                {!isLastValue && (
+                  <button
+                    onClick={handleClickNext}
+                    className="absolute right-3 top-1/2 z-10 -translate-y-1/2 text-3xl text-white md:right-5 xl:right-8"
+                  >
+                    {">"}
+                  </button>
+                )}
               </CustomImage>
             </motion.div>
           </AnimatePresence>
