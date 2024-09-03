e02867cf46f5689081bc1bf2bc28f2095e0cd703
NiNinanana <yeen00123@gmail.com>
2024-09-03 10:56:22 +0900

    feat: shadcn ui 셋팅 및 배경색 수정

--- packages/theme/src/tailwindConfig.ts
+++ packages/theme/src/tailwindConfig.ts
@@ -6,8 +6,8 @@ import { fontFamily } from "./theme/fonts";
 export const tailwindConfig: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    colors,
     extend: {
+      colors,
       fontFamily,
     },
   },
