f03bcdb713c22efb1e97edde144cfce4e07ce2ae
NiNinanana <yeen00123@gmail.com>
2024-11-08 14:45:06 +0900

    refactor: 음악회 모달 대신 새생명축제 모달로 수정

--- apps/client/src/components/modal/concert.tsx
+++ apps/client/src/components/modal/concert.tsx
@@ -4,7 +4,7 @@ const ConcertModal = () => {
   return (
     <div>
       <div className="relative h-[300px] md:h-[500px]">
-        <Image src="/images/concert.jpeg" alt="가을 음악회" fill={true} className="object-contain" />
+        <Image src="/images/new_beginning.jpg" alt="새생명 축제" fill={true} className="object-contain" />
       </div>
     </div>
   );
