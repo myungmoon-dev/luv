4112f5087ba29101ef857f417b8c92238372bfce
강선우 <74632731+NiNinanana@users.noreply.github.com>
2025-01-26 23:51:42 +0900

    Refactor/db migration admin client (#204)
    * refactor: 유튜브 부분 수정
    
    * refactor: 앨범 수정
    
    * refactor: 사용하지 않는 admin api 삭제

--- apps/admin/src/query/youtube/index.ts
+++ apps/admin/src/query/youtube/index.ts
@@ -1,16 +1,15 @@
-import { getLive, getYoutubeLink, postYoutubeLink, putLive } from "@/api/youtube";
+import { getLive, getYoutube, postYoutube, putLive } from "@/api/youtube";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { YoutubeType } from "type";
 import youtubeKeys from "./keys";
 
-// FIXME: youtube 객체를 항상 1개를 가져옴
 export const useGetYoutubeLink = (type: YoutubeType) => {
   const queryKey = youtubeKeys[type]();
 
   return useQuery({
     queryKey,
-    queryFn: async () => await getYoutubeLink(type),
-    select: (response) => response.youtubeList[0].videoId,
+    queryFn: async () => await getYoutube(type),
+    select: (res) => res.videos[0],
   });
 };
 
@@ -19,7 +18,7 @@ export const usePostYoutubeLink = (type: YoutubeType) => {
   const queryKey = youtubeKeys[type]();
 
   return useMutation({
-    mutationFn: postYoutubeLink,
+    mutationFn: postYoutube,
     onSuccess: async () =>
       await queryClient.invalidateQueries({
         queryKey,
