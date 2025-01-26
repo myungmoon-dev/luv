2d44e5da65217a6a212d4a39865ae1c7a9d33e9b
wannabeing <wannabeing@naver.com>
2025-01-27 04:58:57 +0900

    chore: 점검 모달 삭제

--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -1,12 +1,9 @@
 import HomePage from "@/components/home";
 import HomeBanner, { HomeBannerEnum } from "@/components/home/banner";
 import Layout from "@/components/layout";
-import MigrationModal from "@/components/modal/migration";
-import useModalStore from "@/store/modal";
 
 import { generateBlurDataURL } from "@/utils/generateBlurDataURL";
 import path from "path";
-import { useEffect } from "react";
 
 export async function getStaticProps() {
   const watchwordPath = path.resolve("public/images/home/banner-2025.jpg");
@@ -36,12 +33,6 @@ interface IHomePageProps {
 }
 
 export default function Home({ bannerBlurDataURLs }: IHomePageProps) {
-  const open = useModalStore((state) => state.open);
-
-  useEffect(() => {
-    open(<MigrationModal />);
-  }, []);
-
   return (
     <Layout
       customBanner={<HomeBanner blurDataURLs={bannerBlurDataURLs} />}
