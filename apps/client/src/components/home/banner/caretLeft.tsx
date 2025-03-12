fcf35de5f003633722d4daf2a270aaa3a481ad1e
NiNinanana <yeen00123@gmail.com>
2025-03-12 17:51:40 +0900

    refactor: home banner 수정

---
+++ apps/client/src/components/home/banner/caretLeft.tsx
@@ -0,0 +1,15 @@
+const CaretLeftIcon = () => {
+  return (
+    <svg width="13" height="14" viewBox="0 0 13 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M8.125 11.0625L4.0625 7L8.125 2.9375"
+        stroke="#555555"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default CaretLeftIcon;
