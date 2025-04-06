3f71acff7292f15eba1fbdd772724a77b6353cbd
wannabeing <wannabeing@naver.com>
2025-04-06 21:52:47 +0900

    refactor: AlbumSection 컴포넌트 API 적용

--- apps/client/src/components/home/section/album/index.tsx
+++ apps/client/src/components/home/section/album/index.tsx
@@ -1,10 +1,28 @@
 import useResponsive from "@/hooks/useResponsive";
-import { Icon } from "ui";
+import { Icon, Spinner } from "ui";
 import LgImage from "./lg";
 import MdImage from "./md";
+import { Suspense } from "@suspensive/react";
+import { useGetAlbumListSuspense } from "@/query/album";
 
 const HomeAlbumSection = () => {
+  return (
+    <Suspense
+      clientOnly
+      fallback={
+        <div className="flex h-full w-full items-center justify-center">
+          <Spinner />
+        </div>
+      }
+    >
+      <HomeAlbumSectionMain />
+    </Suspense>
+  );
+};
+const HomeAlbumSectionMain = () => {
   const { isSm, isMd, isLg } = useResponsive();
+  const { data: main } = useGetAlbumListSuspense("all");
+  console.log(main);
 
   const getIconSizeNumber = () => {
     if (isMd || isLg) return 14;
@@ -13,23 +31,22 @@ const HomeAlbumSection = () => {
   };
 
   const getImages = () => {
-    // TODO: 앨범 api 적용
-    return new Array(12).fill("/images/sketch-2.jpg");
+    return main.albums;
   };
 
   if (isLg)
     return (
-      <div className="flex flex-col gap-[55px] bg-[#F5F5F5] pb-[167px] pt-[85px]">
-        <div className="flex justify-between px-[125px]">
+      <div className="flex flex-col gap-[55px] bg-[#F5F5F5] pb-[167px] pl-[22px] pt-[85px] sm:pl-[35px] md:px-[30px] lg:px-[125px]">
+        <div className="flex justify-between">
           <p className="text-[30px] font-bold text-[#222222]">명문교회 앨범</p>
           <button className="flex items-center justify-center gap-2">
             <p className="text-[14px]">자세히 보기</p>
             <Icon name="CaretRight" sizeNumber={getIconSizeNumber()} />
           </button>
         </div>
-        <div className="flex overflow-scroll">
+        <div className="flex gap-2 overflow-scroll">
           {getImages().map((image, idx) => (
-            <LgImage imageSrc={image} key={idx} />
+            <LgImage imageSrc={image.imageUrls[0]} key={idx} />
           ))}
         </div>
       </div>
@@ -44,10 +61,12 @@ const HomeAlbumSection = () => {
           <Icon name="CaretRight" sizeNumber={getIconSizeNumber()} />
         </button>
       </div>
-      <div className="grid grid-cols-3 gap-[17px] sm:gap-[12px] md:gap-[10px]">
-        {getImages().map((image, idx) => (
-          <MdImage imageSrc={image} key={idx} />
-        ))}
+      <div className="grid grid-cols-3 gap-[5px] sm:gap-[12px] md:gap-[10px]">
+        {getImages()
+          .slice(0, 9)
+          .map((image, idx) => (
+            <MdImage imageSrc={image.imageUrls[0]} key={idx} />
+          ))}
       </div>
     </div>
   );
