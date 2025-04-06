b2915340e49d0d3326bed77586b62c648a231785
강선우 <74632731+NiNinanana@users.noreply.github.com>
2025-04-06 09:50:51 +0900

    fix: 주보가 여러 개 업로드 가능하고 로딩 처리 잘되도록 수정 (#218)

--- apps/client/src/components/blurImage.tsx
+++ apps/client/src/components/blurImage.tsx
@@ -1,19 +1,34 @@
 import { useGetBlurImage } from "@/query/blurImage";
 import Image from "next/image";
+import { Dispatch, SetStateAction, useState } from "react";
+import { Spinner } from "ui";
 
 interface IBlurImageProps {
   img: string;
   alt: string;
   fill?: boolean;
+  isLoading: boolean;
+  setIsLoading: Dispatch<SetStateAction<boolean>>;
 }
 
-const BlurImageComponent = ({ img, alt, fill = false }: IBlurImageProps) => {
+const BlurImageComponent = ({ img, alt, fill = false, isLoading, setIsLoading }: IBlurImageProps) => {
   // FIXME: useGetBlurImage 수정 해야함
   // const { data } = useGetBlurImage({
   //   img,
   // });
 
-  return <>{<Image src={img} fill={fill} alt={alt} />}</>;
+  return (
+    <div className="relative">
+      {isLoading && (
+        <div className="absolute left-1/2 top-1/2 z-10 flex h-[220px] w-[338px] -translate-x-1/2 -translate-y-1/2 items-center justify-center sm:h-[300px] sm:w-[468px] md:h-[440px] md:w-[688px] lg:h-[550px] lg:w-[868px]">
+          <Spinner />
+        </div>
+      )}
+      <div className="relative h-[220px] w-[288px] sm:h-[300px] sm:w-[468px] md:h-[440px] md:w-[688px] lg:h-[550px] lg:w-[868px]">
+        {<Image src={img} fill={fill} alt={alt} onLoad={() => setIsLoading(false)} />}
+      </div>
+    </div>
+  );
 };
 
 export default BlurImageComponent;
