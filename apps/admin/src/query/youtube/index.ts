2e273dbc7f3605c0bff2b2711ac7955c4110e111
NiNinanana <yeen00123@gmail.com>
2025-05-29 21:40:39 +0900

    feat: video update, delete 기능 추가

--- apps/admin/src/query/youtube/index.ts
+++ apps/admin/src/query/youtube/index.ts
@@ -1,7 +1,16 @@
-import { getLive, getYoutube, postYoutube, putLive } from "@/api/youtube";
+import { deleteVideo, getLive, getYoutube, postYoutube, putLive, putVideo } from "@/api/youtube";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { YoutubeType } from "type";
 import youtubeKeys from "./keys";
+
+export const useGetVideos = ({ type }: { type: YoutubeType }) => {
+  const queryKey = youtubeKeys[type]();
+
+  return useQuery({
+    queryKey,
+    queryFn: () => getYoutube(type),
+  });
+};
 
 export const useGetYoutubeLink = (type: YoutubeType) => {
   const queryKey = youtubeKeys[type]();
@@ -36,3 +45,7 @@ export const useGetLive = () => {
 };
 
 export const usePutLive = () => useMutation({ mutationFn: putLive });
+
+export const useDeleteVideo = () => useMutation({ mutationFn: deleteVideo });
+
+export const usePutVideo = () => useMutation({ mutationFn: putVideo });
