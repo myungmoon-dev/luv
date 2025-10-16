5e9d9d8b5a1b1a515bb30554ae7943ed776bdb2a
wannabeing <wannabeing@naver.com>
2025-10-17 05:09:51 +0900

    refactor: 다음세대 메인페이지 디자인 리팩토링

--- apps/client/src/pages/education/index.tsx
+++ apps/client/src/pages/education/index.tsx
@@ -1,8 +1,24 @@
 import EducationCoreValuesSection from "@/components/education/section/corevalues";
+import EducationMenusSection from "@/components/education/section/menus";
 import EducationMissionSection from "@/components/education/section/mission";
 import EducationVisionSection from "@/components/education/section/vision";
 import Layout from "@/components/layout";
 import { educationInnerMenus } from "@/constants/innerMenus/education";
+import { Icon } from "ui";
+
+const EducationMenuTitle = () => {
+  return (
+    <div className="flex w-full items-center justify-between">
+      <p className="text-[22px] font-bold text-[#222222] sm:text-[25px] lg:text-[30px]">다음세대</p>
+
+      {/* FIXME: 앨범 넘어가야 함 */}
+      <button onClick={() => {}} className="flex items-center justify-center gap-2 rounded-md bg-[#001F54] px-3 py-2">
+        <p className="text-sm font-medium text-white">앨범보기</p>
+        <Icon name="CaretRight" sizeNumber={14} cursor="ui-cursor-pointer" />
+      </button>
+    </div>
+  );
+};
 
 const EducationPage = () => {
   return (
@@ -13,10 +29,19 @@ const EducationPage = () => {
       bannerImage="/images/education/banner.jpg"
       innerMenus={educationInnerMenus}
     >
-      <div className="mb-20 flex w-full flex-col items-center justify-center gap-10 overflow-hidden md:gap-16 lg:gap-20">
+      <div className="flex w-full flex-col gap-10 overflow-hidden md:gap-16 lg:gap-20">
+        {/* 1. 사명선언문 섹션 */}
         <EducationMissionSection />
-        <EducationVisionSection />
-        <EducationCoreValuesSection />
+
+        <div className="flex flex-col gap-8 px-5">
+          {/* 2. 다음세대 메뉴 섹션  */}
+          <EducationMenuTitle />
+          <EducationMenusSection />
+          {/* 3. 비전 섹션 */}
+          <EducationVisionSection />
+          {/* 4. 핵심가치 섹션 */}
+          <EducationCoreValuesSection />
+        </div>
       </div>
     </Layout>
   );
