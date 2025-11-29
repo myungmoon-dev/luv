763b2b1e71316e0b296492b7cf83433db96a8a61
NiNinanana <yeen00123@gmail.com>
2025-11-29 23:54:06 +0900

    fix: main 페이지 앨범 제거

--- apps/client/src/components/home/index.tsx
+++ apps/client/src/components/home/index.tsx
@@ -4,11 +4,11 @@ import VideosSection from "./section/videos";
 
 const HomePage = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col pb-12 md:pb-20 lg:pb-40">
       <NavSection />
       <VideosSection />
-      <div className="h-[8px] w-full bg-[#E6E6E6]" />
-      <HomeAlbumSection />
+      {/* <div className="h-[8px] w-full bg-[#E6E6E6]" />
+      <HomeAlbumSection /> */}
     </div>
   );
 };
