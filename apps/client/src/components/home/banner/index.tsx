1f8174458c8b1e7224e497a721e958043cbdf665
wannabeing <wannabeing@naver.com>
2026-01-05 23:06:28 +0900

    chore: 2026 배너 이미지 변경

--- apps/client/src/components/home/banner/index.tsx
+++ apps/client/src/components/home/banner/index.tsx
@@ -60,15 +60,15 @@ const HomeBanner = () => {
   return (
     <div className="relative">
       <div className="relative h-[474.23px] w-auto sm:h-[398px] md:h-[368px] lg:h-[676px]">
-        <Image src="/images/home/banner-2025.jpg" alt="banner" fill={true} objectFit="cover" />
+        <Image src="/images/home/banner-2026.jpeg" alt="banner" fill={true} objectFit="cover" />
       </div>
-      <div className="absolute left-[35px] top-[195px] flex flex-col gap-2.5 text-white sm:left-[65px] sm:top-[144px] sm:gap-1.5 md:top-[173px] md:gap-1 lg:left-[182px] lg:top-[344px] lg:gap-2.5">
+      <div className="absolute left-[35px] top-[180px] flex flex-col gap-2.5 text-white sm:left-[65px] sm:top-[144px] sm:gap-1.5 md:top-[173px] md:gap-1 lg:left-[182px] lg:top-[250px] lg:gap-2.5">
         <p className="text-[26px] font-black leading-[26px] sm:text-[27px] sm:leading-[28px] md:text-[29px] md:leading-[30px] lg:text-[42px] lg:font-bold lg:leading-[50px]">
-          보라
+          일어나라
           <br />
-          내가 반드시 길을 내리라
+          빛을 발하라
         </p>
-        <p className="text-[18px] font-bold sm:text-[20px] lg:text-[24px]">이사야 43:19</p>
+        <p className="text-[18px] font-bold sm:text-[20px] lg:text-[24px]">이사야 60:1</p>
       </div>
       <div className="absolute bottom-0 flex w-full items-center bg-[#F6F6F6] bg-opacity-[30%] py-[18px] sm:py-[26px] md:static lg:hidden">
         <div
@@ -78,7 +78,11 @@ const HomeBanner = () => {
           <CaretLeftIcon />
         </div>
         {currentBanner === HomeBannerEnum.Live && (
-          <HomeBannerSmCard description="명문교회 예배 생방송" handleClick={handleOpenLive} image="onair" />
+          <HomeBannerSmCard
+            description="명문교회 예배 생방송"
+            handleClick={handleOpenLive}
+            image="onair"
+          />
         )}
         {currentBanner === HomeBannerEnum.Bible && (
           <HomeBannerSmCard
@@ -108,7 +112,11 @@ const HomeBanner = () => {
           image="bible"
         />
         <div className="h-[92px] w-[1px] bg-white" />
-        <HomeBannerLgCard description="명문교회 예배 생방송" handleClick={handleOpenLive} image="onair" />
+        <HomeBannerLgCard
+          description="명문교회 예배 생방송"
+          handleClick={handleOpenLive}
+          image="onair"
+        />
         <div className="h-[92px] w-[1px] bg-white" />
         <HomeBannerLgCard
           description={"전 성도가 함께하는\n맛있는 가정예배"}
