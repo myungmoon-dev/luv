3efdfdaf52884c401c2083f790668ca6a4f5043c
ChoiHyuk <wannabeing@naver.com>
2026-06-08 17:00:03 +0900

    Chore: 빌드시 환경변수값 못불러오는 오류 해결 (#274)
    * chore: 빌드시 pnpm 캐시삭제
    
    * chore: 배포오류수정
    
    * chore: 빌드 오류수정
    
    * chore: 어드민 페이지 API 주소설정
    
    * chore: 사용자페이지 API 주소설정
    
    * chore: 빌드시 환경변수값 못불러오는 오류해결

--- apps/admin/next.config.mjs
+++ apps/admin/next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_AXIOS_DEFAULT_BASEURL}/api/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_AXIOS_DEFAULT_BASEURL || "http://localhost:8080"}/api/:path*`,
       },
     ];
   },
