55c4b0cb8747be0711223a8be35ecd3d2a81d43c
wannabeing <wannabeing@naver.com>
2026-06-08 16:30:41 +0900

    chore: 어드민 페이지 API 주소설정

--- apps/admin/next.config.mjs
+++ apps/admin/next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8080/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_AXIOS_DEFAULT_BASEURL}/api/:path*`,
       },
     ];
   },
