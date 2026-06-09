dd1807856b65bae87768d68763cc30b5a10261f4
wannabeing <wannabeing@naver.com>
2026-06-09 23:11:44 +0900

    chore: 관리자페이지 title 설정

--- apps/admin/src/pages/_document.tsx
+++ apps/admin/src/pages/_document.tsx
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        <title>명문교회 관리자페이지</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
