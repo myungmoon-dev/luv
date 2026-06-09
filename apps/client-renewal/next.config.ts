16180f0491a98fbdfbece5dba56b5d03a0d7bdd8
wannabeing <wannabeing@naver.com>
2026-06-09 13:12:04 +0900

    chore: 브라우저가 NextJS 거쳐서 API 호출하도록 변경

--- apps/client-renewal/next.config.ts
+++ apps/client-renewal/next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_AXIOS_DEFAULT_BASEURL || "http://localhost:8080"}/api/:path*`,
+      },
+    ];
+  },
   async redirects() {
     return [
       {
