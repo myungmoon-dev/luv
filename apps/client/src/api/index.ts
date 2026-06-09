566fcd8614aba7699636b5463657efa14f0b49bd
wannabeing <wannabeing@naver.com>
2026-06-09 10:32:42 +0900

    chore: mixed HTTP solved

--- apps/client/src/api/index.ts
+++ apps/client/src/api/index.ts
@@ -5,7 +5,7 @@ const origin = process.env.NEXT_PUBLIC_AXIOS_DEFAULT_BASEURL?.replace(/\/$/, "")
 export const isClientApiConfigured = () => origin.length > 0;
 
 export const api = axios.create({
-  baseURL: origin ? `${origin}/api` : "",
+  baseURL: "/api",
 });
 
 api.interceptors.response.use((response) => {
